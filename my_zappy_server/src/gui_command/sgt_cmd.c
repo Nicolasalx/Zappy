16bc4d9ca3f77c718786ad337aab6c53259341c6
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-21 16:24:58 +0200

    add: gui command sgt to get the frequency

---
+++ my_zappy_server/src/gui_command/sgt_cmd.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** sgt_cmd
+*/
+
+#include "zappy_server.h"
+
+void sgt_cmd(int nb_args, char **argv, client_t *client, server_t *server)
+{
+    char buffer[100] = {0};
+    snprintf(buffer, sizeof(buffer), "sgt %d\n", server->freq);
+    send_msg_client(client->fd, buffer);
+}
