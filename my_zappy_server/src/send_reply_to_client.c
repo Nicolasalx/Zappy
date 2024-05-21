4ae51baaf4f27242dd1f6ed2f10142040365d6d6
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-21 23:19:08 +0200

    fix: eroorwith fd is set

--- my_zappy_server/src/send_reply_to_client.c
+++ my_zappy_server/src/send_reply_to_client.c
@@ -9,9 +9,9 @@
 
 void send_msg_client(int fd, char *reply)
 {
-    if (FD_ISSET(fd, &get_server(NULL)->write_set)) {
+    //if (FD_ISSET(fd, &get_server(NULL)->write_set)) {
         if (send(fd, reply, strlen(reply), 0) == -1) {
             dprintf(2, RED("Fail to send reply to client.\n"));
         }
-    }
+    //}
 }
