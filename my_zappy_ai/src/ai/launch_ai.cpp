9c587c99ff07649db72725293ad3202321bb6c26
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-23 19:33:19 +0200

    add: setup of the engine of the ai part of the project has been added

---
+++ my_zappy_ai/src/ai/launch_ai.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** launch_ai
+*/
+
+#include "zappy_ai.hpp"
+
+void Ai::Client::launchAi()
+{
+    std::cout << "launch ai" << std::endl;
+
+    // while (1) {
+    //     for (auto &player : this->gameState.players_list) {
+    //         std::cout << "Player " << player.n << " is at position " << player.pos.x << " " << player.pos.y << " and is level " << player.level << std::endl;
+    //     }
+    //     sleep(5);
+    // }
+}
