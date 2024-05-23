b676b06005f446a0bbe8a5c6164441f6530cd5f4
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-23 19:37:41 +0200

    add: function to init new client with a setup in the part ai has been added

---
+++ my_zappy_ai/src/client/network/initClientSet.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** init_client_set
+*/
+
+#include "zappy_ai.hpp"
+
+void Ai::Client::initClientSet()
+{
+    FD_ZERO(&this->readSet);
+    FD_SET(STDIN_FILENO, &this->readSet);
+    FD_SET(this->fd, &this->readSet);
+    FD_ZERO(&this->writeSet);
+    FD_SET(this->fd, &this->writeSet);
+    this->maxFd = this->fd;
+}
+
+void Ai::Client::monitorInput()
+{
+    if (select(maxFd + 1, &this->readSet,
+        &this->writeSet, NULL, NULL) == -1) {
+        throw my::tracked_exception("Select failed.\n");
+    }
+}
