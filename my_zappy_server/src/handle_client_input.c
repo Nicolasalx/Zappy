0890c54c2bb48ac5b9759a43d8b57d8f5aff76b3
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-23 12:29:25 +0200

    add: coding style to the handle client input function

--- my_zappy_server/src/handle_client_input.c
+++ my_zappy_server/src/handle_client_input.c
@@ -7,6 +7,31 @@
 
 #include "zappy_server.h"
 
+static void handle_new_graphic_client(server_t *server, client_t *client)
+{
+    client->is_graphic = true;
+    msz_cmd(0, NULL, client, server);
+    sgt_cmd(0, NULL, client, server);
+    mct_cmd(0, NULL, client, server);
+    tna_cmd(0, NULL, client, server);
+}
+
+static void handle_new_player_client(server_t *server, client_t *client, char *cmd)
+{
+    char buff[MAX_TEAMNAME_SIZE + 2] = {0};
+
+    for (int i = 0; i < server->team_count; ++i) {
+        strcpy(buff, server->team_list[i].name);
+        strcat(buff, "\n");
+        if (strcmp(cmd, buff) == 0) {
+            client->player.team = &server->team_list[i];
+            init_player(client, server);
+            return;
+        }
+    }
+    send_msg_client(client->fd, "ko (not logged in)\n");
+}
+
 void handle_client_input(server_t *server, client_t *client, char *cmd)
 {
     char buffer[100] = {0};
@@ -19,27 +44,9 @@ void handle_client_input(server_t *server, client_t *client, char *cmd)
         handle_ai_input(server, client, cmd);
     } else {
         if (strcmp(cmd, "GRAPHIC\n") == 0) {
-            client->is_graphic = true;
-            msz_cmd(0, NULL, client, server);
-            sgt_cmd(0, NULL, client, server);
-            mct_cmd(0, NULL, client, server);
-            tna_cmd(0, NULL, client, server);
+            handle_new_graphic_client(server, client);
         } else {
-            for (int i = 0; i < server->team_count; ++i) {
-                if (strlen(cmd) > 1 && strncmp(cmd, server->team_list[i].name, strlen(cmd) - 1) == 0) {
-                    client->player.team = &server->team_list[i];
-                    init_player(client, server);
-                    for (int i = 0; i < MAX_CLIENT; i++) {
-                        if (server->clients[i].is_graphic == true) {
-                            snprintf(buffer, sizeof(buffer), "pnw %d %d %d %d %d %s\n", client->player.id, client->player.pos_x, client->player.pos_y, client->player.orientation[NORTH], client->player.level, client->player.team->name);
-                            send_msg_client(server->clients[i].fd, buffer);
-                            memset(buffer, 0, sizeof(buffer));
-                        }
-                    }
-                    return;
-                }
-            }
-            send_msg_client(client->fd, "ko (not logged in)\n");
+            handle_new_player_client(server, client, cmd);
         }
     }
 }
