3ec63539fc2261c1624740808628aa15026a7801
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-23 19:34:26 +0200

    add: all the main function to parse the ai system has been added

---
+++ my_zappy_ai/src/client/network/parseArgs/checkArgValidity.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** B-NWP-400-PAR-4-1-myftp-thibaud.cathala
+** File description:
+** check_arg_validity
+*/
+
+#include "zappy_ai.hpp"
+
+void Ai::Client::getArgs(int argc, const char **argv)
+{
+    if (argc < 2) {
+        throw my::tracked_exception("To few arguments gived.\n");
+    }
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "-help") == 0) {
+            displayHelp();
+        } else if (std::strcmp(argv[i], "-p") == 0) {
+            getPort(argc, argv, i);
+        } else if (std::strcmp(argv[i], "-n") == 0) {
+            getTeamName(argc, argv, i);
+        } else if (std::strcmp(argv[i], "-h") == 0) {
+            analyseIP(argc, argv, i);
+        }
+    }
+}
+
+Ai::Client::Client(int argc, const char **argv)
+{
+    this->ip = "127.0.0.1";
+    this->port = 8080;
+    this->getArgs(argc, argv);
+    if (inet_aton(this->ip.c_str(), &this->serverAddress.sin_addr) == 0) {
+        throw my::tracked_exception("Invalid Ip.\n");
+    }
+    std::thread graphic_thread(&Client::launchAi, this);
+    graphic_thread.detach();
+}
