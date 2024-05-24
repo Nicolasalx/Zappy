1a13b0e38a3d3428b70724ce789e26a23f0e7a51
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-05-24 18:12:13 +0200

    add: object rendering and player orientation support

--- my_zappy_gui/src/graphic/graphic_draw.cpp
+++ my_zappy_gui/src/graphic/graphic_draw.cpp
@@ -17,7 +17,36 @@ void Graphic::draw_sky_box()
     rlEnableDepthMask();
 }
 
-void Graphic::draw_3D_models()
+void Graphic::draw_map()
 {
-    DrawModel(this->model_list[0], (Vector3){0, 1, 0}, 4.0f, WHITE);
+    for (int i = 0; i < this->gameState->map_size.x; i++) {
+        for (int j = 0; j < this->gameState->map_size.y; j++) {
+            DrawModel(this->model_list[ISLAND], (Vector3){i * SCALE, -1.5f, j * SCALE}, 1.0f, GREEN);
+        }
+    }
+}
+
+void Graphic::draw_object()
+{
+    for (int i = 0; i < 7; i++) {
+        for (auto &food : this->gameState->object_pos[i]) {
+            DrawModel(this->model_list[i], (Vector3){food.x * SCALE + object_padding[i][0], 0.5f, food.y * SCALE + object_padding[i][0]}, 1.0f, RED);
+        }
+    }
+}
+
+void Graphic::draw_player()
+{
+    for (auto &player : this->gameState->players_list) {
+        //DrawModel(this->model_list[PLAYER], (Vector3){player.pos.x * SCALE, 0.5f, player.pos.y * SCALE}, 1.0f, BLUE);
+        std::cout << player.orientation << std::endl;
+        if (player.orientation == 1)
+            DrawModelEx(this->model_list[PLAYER], (Vector3){player.pos.x * SCALE, 0.5f, player.pos.y * SCALE}, (Vector3){0, 1, 0}, 180, (Vector3){1, 1, 1}, WHITE);
+        else if (player.orientation == 2)
+            DrawModelEx(this->model_list[PLAYER], (Vector3){player.pos.x * SCALE, 0.5f, player.pos.y * SCALE}, (Vector3){0, 1, 0}, 90, (Vector3){1, 1, 1}, WHITE);
+        else if (player.orientation == 3)
+            DrawModelEx(this->model_list[PLAYER], (Vector3){player.pos.x * SCALE, 0.5f, player.pos.y * SCALE}, (Vector3){0, 1, 0}, 0, (Vector3){1, 1, 1}, WHITE);
+        else if (player.orientation == 4)
+            DrawModelEx(this->model_list[PLAYER], (Vector3){player.pos.x * SCALE, 0.5f, player.pos.y * SCALE}, (Vector3){0, 1, 0}, 270, (Vector3){1, 1, 1}, WHITE);
+    }
 }
--- my_zappy_gui/src/graphic/graphic_init.cpp
+++ my_zappy_gui/src/graphic/graphic_init.cpp
@@ -7,14 +7,18 @@
 
 #include "zappy_gui.hpp"
 
-Graphic::Graphic()
+Graphic::Graphic(GameState *gameState)
 {
+    this->gameState = gameState;
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Zappy GUI");
     this->init_camera();
     this->set_fps(60);
     this->init_sky_box();
+    this->init_object();
+    this->init_island();
     this->init_player();
+    this->init_object_padding();
 }
 
 Graphic::~Graphic()
@@ -56,17 +60,60 @@ void Graphic::init_sky_box()
 
 void Graphic::init_island()
 {
-    ;
+    //avec un cube pour l'instant
+    Mesh cube = GenMeshCube(3.0f, 3.0f, 3.0f);
+    Model island = LoadModelFromMesh(cube);
+    this->model_list.push_back(island);
 }
 
 void Graphic::init_player()
 {
     Model player = LoadModel("assets/model.iqm");
-    this->model_list.push_back(player);
-    int animsCount = 1;
-    ModelAnimation *anims = LoadModelAnimations("assets/model.iqm", &animsCount);
     Texture2D texture = LoadTexture("assets/test_texture.png");
-    SetMaterialTexture(&this->model_list[0].materials[0], MATERIAL_MAP_DIFFUSE, texture);
-    this->animation_list.push_back(anims);
-    this->model_list[0].transform = MatrixRotateXYZ((Vector3){-90.0f * (PI / 180), 0.0f, 0.0f});
+    SetMaterialTexture(&player.materials[0], MATERIAL_MAP_DIFFUSE, texture);
+    player.transform = MatrixRotateXYZ((Vector3){-90.0f * (PI / 180), 0.0f, 0.0f});
+    this->model_list.push_back(player);
+
+    int animsCount = 1;
+    this->player_animation = LoadModelAnimations("assets/model.iqm", &animsCount);
+}
+
+void Graphic::init_object()
+{
+    // food
+    Mesh sphere = GenMeshSphere(0.5f, 32, 32);
+    Model food = LoadModelFromMesh(sphere);
+    this->model_list.push_back(food);
+    // limenate
+    Mesh limenate = GenMeshCylinder(0.5f, 1.0f, 32);
+    Model limenate_model = LoadModelFromMesh(limenate);
+    this->model_list.push_back(limenate_model);
+    // deraumere
+    Mesh deraumere = GenMeshCone(0.5f, 1.0f, 32);
+    Model deraumere_model = LoadModelFromMesh(deraumere);
+    this->model_list.push_back(deraumere_model);
+    // sibur
+    Mesh sibur = GenMeshTorus(0.5f, 1.0f, 32, 32);
+    Model sibur_model = LoadModelFromMesh(sibur);
+    this->model_list.push_back(sibur_model);
+    // mendiane
+    Mesh mendiane = GenMeshCube(0.5f, 1.0f, 1.0f);
+    Model mendiane_model = LoadModelFromMesh(mendiane);
+    this->model_list.push_back(mendiane_model);
+    // phiras
+    Mesh phiras = GenMeshCylinder(0.5f, 1.0f, 32);
+    Model phiras_model = LoadModelFromMesh(phiras);
+    this->model_list.push_back(phiras_model);
+    // thystame
+    Mesh thystame = GenMeshCylinder(0.5f, 1.0f, 32);
+    Model thystame_model = LoadModelFromMesh(thystame);
+    this->model_list.push_back(thystame_model);
+}
+
+void Graphic::init_object_padding()
+{
+    for (int i = 0; i < 7; i++) {
+        this->object_padding[i][0] = cos((PI / 180.0f) * (i * 50.0f));
+        this->object_padding[i][1] = sin((PI / 180.0f) * (i * 50.0f));
+    }
 }
