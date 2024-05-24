9b82cf268d6ff26ada586ec81f6fd985c77739de
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-24 13:22:58 +0200

    add: ability to see player throught look command

--- my_zappy_server/src/ai_command/look_cmd.c
+++ my_zappy_server/src/ai_command/look_cmd.c
@@ -7,12 +7,23 @@
 
 #include "zappy_server.h"
 
-static void get_item_str(char *buff, int *item)
+static void get_item_str(char *buff, int x, int y, server_t *server)
 {
     bool first = true;
 
+    for (int i = 0; i < MAX_CLIENT; ++i) {
+        if (server->clients[i].fd != 0
+            && server->clients[i].player.pos_x == x
+            && server->clients[i].player.pos_y == y) {
+            if (!first) {
+                strcat(buff, " ");
+            }
+            strcat(buff, "player");
+            first = false;
+        }
+    }
     for (int i = 0; i < NB_ITEM; ++i) {
-        for (int j = 0; j < item[i]; ++j) {
+        for (int j = 0; j < server->world.map[y][x].item[i]; ++j) {
             if (!first) {
                 strcat(buff, " ");
             }
@@ -29,9 +40,8 @@ static void look_north(client_t *client, server_t *server)
     strcpy(buff, "[");
     for (int i = 0; i <= client->player.level; ++i) {
         for (int x = (client->player.pos_x - i); x <= (client->player.pos_x + i); ++x) {
-            get_item_str(buff, server->world.map
-                [(client->player.pos_y - i + server->world.size_y) % server->world.size_y]
-                [(x + server->world.size_x) % server->world.size_x].item);
+            get_item_str(buff, (x + server->world.size_x) % server->world.size_x,
+                (client->player.pos_y - i + server->world.size_y) % server->world.size_y, server);
             if (x < (client->player.pos_x + i) || i < client->player.level) {
                 strcat(buff, ",");
             }
@@ -48,9 +58,9 @@ static void look_south(client_t *client, server_t *server)
     strcpy(buff, "[");
     for (int i = 0; i <= client->player.level; ++i) {
         for (int x = (client->player.pos_x - i); x <= (client->player.pos_x + i); ++x) {
-            get_item_str(buff, server->world.map
-                [(client->player.pos_y + i) % server->world.size_y]
-                [(x + server->world.size_x) % server->world.size_x].item);
+            get_item_str(buff,
+                (x + server->world.size_x) % server->world.size_x,
+                (client->player.pos_y + i) % server->world.size_y, server);
             if (x < (client->player.pos_x + i) || i < client->player.level) {
                 strcat(buff, ",");
             }
@@ -67,9 +77,9 @@ static void look_east(client_t *client, server_t *server)
     strcpy(buff, "[");
     for (int i = 0; i <= client->player.level; ++i) {
         for (int y = (client->player.pos_y - i); y <= (client->player.pos_y + i); ++y) {
-            get_item_str(buff, server->world.map
-                [(y + server->world.size_y) % server->world.size_y]
-                [(client->player.pos_x + i) % server->world.size_x].item);
+            get_item_str(buff,
+                (client->player.pos_x + i) % server->world.size_x,
+                (y + server->world.size_y) % server->world.size_y, server);
             if (y < (client->player.pos_y + i) || i < client->player.level) {
                 strcat(buff, ",");
             }
@@ -86,9 +96,8 @@ static void look_west(client_t *client, server_t *server)
     strcpy(buff, "[");
     for (int i = 0; i <= client->player.level; ++i) {
         for (int y = (client->player.pos_y - i); y <= (client->player.pos_y + i); ++y) {
-            get_item_str(buff, server->world.map
-                [(y + server->world.size_y) % server->world.size_y]
-                [(client->player.pos_x - i + server->world.size_x) % server->world.size_x].item);
+            get_item_str(buff, (client->player.pos_x - i + server->world.size_x) % server->world.size_x,
+                (y + server->world.size_y) % server->world.size_y, server);
             if (y < (client->player.pos_y + i) || i < client->player.level) {
                 strcat(buff, ",");
             }
