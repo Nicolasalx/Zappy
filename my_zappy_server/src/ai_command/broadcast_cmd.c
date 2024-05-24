99baa408c42b682e1b089bda1b49e18e1e96d795
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-24 20:18:27 +0200

    fix: resource density

--- my_zappy_server/src/ai_command/broadcast_cmd.c
+++ my_zappy_server/src/ai_command/broadcast_cmd.c
@@ -7,17 +7,50 @@
 
 #include "zappy_server.h"
 
+int calculate_direction(client_t *src, client_t *dest)
+{
+    int dx = dest->player.pos_x - src->player.pos_x;
+    int dy = dest->player.pos_y - src->player.pos_y;
+    int direction = 0;
+
+    if (abs(dx) > abs(dy)) {
+        direction = (dx > 0) ? 3 : 7;  // Right or Left
+    } else {
+        direction = (dy > 0) ? 1 : 5;  // Up or Down
+    }
+
+    if (dx > 0 && dy > 0) direction = 2;
+    if (dx > 0 && dy < 0) direction = 4;
+    if (dx < 0 && dy < 0) direction = 6;
+    if (dx < 0 && dy > 0) direction = 8;
+
+    return direction;
+}
+
 void broadcast_cmd(char *argv, client_t *client, server_t *server)
 {
     char buffer[100] = {0};
     char *message = strtok(argv, "\n");
 
-    snprintf(buffer, sizeof(buffer), "message %d, %s\n", client->player.orientation, message);
     for (int i = 0; i < MAX_CLIENT; ++i) {
-        if (server->clients[i].fd != 0 && server->clients[i].is_graphic != true && server->clients[i].player.team
-            && client->player.id != server->clients[i].player.id) {
+        if (server->clients[i].fd != 0 && server->clients[i].is_graphic != true
+        && server->clients[i].player.team && client->player.id != server->clients[i].player.id) {
+            int direction = calculate_direction(client, &server->clients[i]);
+            snprintf(buffer, sizeof(buffer), "message %d, %s\n", direction, message);
             send_msg_client(server->clients[i].fd, buffer);
         }
     }
     pbc_reply(server, client, message);
+
+//    char buffer[100] = {0};
+//    char *message = strtok(argv, "\n");
+//
+//    snprintf(buffer, sizeof(buffer), "message %d, %s\n", client->player.orientation, message);
+//    for (int i = 0; i < MAX_CLIENT; ++i) {
+//        if (server->clients[i].fd != 0 && server->clients[i].is_graphic != true && server->clients[i].player.team
+//            && client->player.id != server->clients[i].player.id) {
+//            send_msg_client(server->clients[i].fd, buffer);
+//        }
+//    }
+//    pbc_reply(server, client, message);
 }
