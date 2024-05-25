9b01d2e56b08284658de184fdd045809c54c296a
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-25 20:33:19 +0200

    add: new header to post command in the ai part

---
+++ include/zappyAI/postCommand.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** postCommand
+*/
+
+#ifndef POSTCOMMAND_H_
+    #define POSTCOMMAND_H_
+
+
+namespace Ai
+{
+
+}
+
+#endif /* !POSTCOMMAND_H_ */
