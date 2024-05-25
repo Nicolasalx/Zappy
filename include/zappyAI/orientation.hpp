07027850f0f0a488c9cd04e9f7c21af0df4db0a7
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-25 22:18:55 +0200

    add: orientation has been added in the ai part

---
+++ include/zappyAI/orientation.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** orientation
+*/
+
+#ifndef ORIENTATION_H_
+    #define ORIENTATION_H_
+
+namespace Ai
+{
+    enum Orientation {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+    };
+}
+
+#endif /* !ORIENTATION_H_ */
