a69df971ae65704d2b5745dff4c9af0ce35ad6e3
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-26 14:38:32 +0200

    del: useless comment on the serve

--- my_zappy_server/src/ai_command/broadcast_cmd.c
+++ my_zappy_server/src/ai_command/broadcast_cmd.c
@@ -46,16 +46,4 @@ void broadcast_cmd(char *argv, client_t *client, server_t *server)
     }
     send_msg_client(client->fd, "ok\n");
     pbc_reply(server, client, message);
-
-//    char buffer[100] = {0};
-//    char *message = strtok(argv, "\n");
-//
-//    snprintf(buffer, sizeof(buffer), "message %d, %s\n", client->player.orientation, message);
-//    for (int i = 0; i < MAX_CLIENT; ++i) {
-//        if (server->clients[i].fd != 0 && server->clients[i].is_graphic != true && server->clients[i].player.team
-//            && client->player.id != server->clients[i].player.id) {
-//            send_msg_client(server->clients[i].fd, buffer);
-//        }
-//    }
-//    pbc_reply(server, client, message);
 }
