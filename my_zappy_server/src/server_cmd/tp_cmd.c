3a26c744999a425067aad9da47b469e6de76aefa
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-26 16:40:33 +0200

    add: tp command to tp a player to given coords

--- my_zappy_server/src/server_cmd/tp_cmd.c
+++ my_zappy_server/src/server_cmd/tp_cmd.c
@@ -7,8 +7,43 @@
 
 #include "zappy_server.h"
 
-void tp_cmd(int argc, char **argv, server_t *server)
+static bool tp_error_management(char **argv, server_t *server)
 {
-    (void) server;
-    printf("argc: %d argv: %s %s %s %s\n", argc, argv[0], argv[1], argv[2], argv[3]);
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    int o = atoi(argv[3]);
+
+    if (!my_str_only_cont(argv[0], "0123456789")
+    || !my_str_only_cont(argv[1], "0123456789")
+    || !my_str_only_cont(argv[2], "0123456789")
+    || !my_str_only_cont(argv[3], "0123456789")
+    || x < 0 || x >= server->world.size_x
+    || y < 0 || y >= server->world.size_y
+    || o < 1 || o > 4) {
+        printf("Error: invalid argument\n");
+        return true;
+    }
+    return false;
+}
+
+void tp_cmd(int, char **argv, server_t *server)
+{
+    int id = atoi(argv[0]);
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    int o = atoi(argv[3]);
+
+    if (tp_error_management(argv, server))
+        return;
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (server->clients[i].fd != 0 && server->clients[i].player.id == id
+        && server->clients[i].is_graphic == false) {
+            server->clients[i].player.pos_x = x;
+            server->clients[i].player.pos_y = y;
+            server->clients[i].player.orientation = o - 1;
+            ppo_reply(server, &server->clients[i]);
+            return;
+        }
+    }
+    printf("Error: client not found\n");
 }
