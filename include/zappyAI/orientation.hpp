5b94226ee64fea279e6807d98d841360c250d407
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-26 12:21:16 +0200

    mod: orientation has been modified to add WEST and EAST at the NORTH and SOUTH

--- include/zappyAI/orientation.hpp
+++ include/zappyAI/orientation.hpp
@@ -12,9 +12,14 @@ namespace Ai
 {
     enum Orientation {
         NORTH,
+        NORTH_EAST,
         EAST,
+        SOUTH_EAST,
         SOUTH,
+        SOUTH_WEST,
         WEST,
+        NORTH_WEST,
+        ACTUAL_PLACE
     };
 }
 
