cbf00bd51507cbe3bf2b8156c43bb0ec243d5880
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-26 12:24:08 +0200

    add: new function to set level to private member of player

---
+++ my_zappy_ai/src/player/setNewLevel.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** setNewLevel
+*/
+
+#include "zappyAi.hpp"
+
+void Ai::Player::setNewLevel(int level)
+{
+    this->_level = level;
+}
