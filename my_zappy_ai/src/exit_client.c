a9de1b30b76aff96d07dbd1213cd017d0b206efd
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-27 09:54:27 +0200

    exit client function

---
+++ my_zappy_ai/src/exit_client.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** exit_client
+*/
+
+#include "zappy_ai.h"
+
+void exit_client(int exit_value, const char *message)
+{
+    if (message != NULL) {
+        my_putstr(message);
+    }
+    delete_client(get_client(NULL));
+    my_exit(exit_value);
+}
