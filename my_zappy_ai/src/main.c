9b58d0539876bc2f8b06acba121fe82ae6b82096
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-27 09:50:03 +0200

    add: client main;c

---
+++ my_zappy_ai/src/main.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** main
+*/
+
+#include "zappy_ai.h"
+
+int main(int argc, char const *argv[])
+{
+    client_t client = {0};
+
+    strcpy(client.team_name, "Team1\n");
+    check_arg_validity(argc, argv, &client);
+    create_client(&client);
+    lauch_client(&client);
+    delete_client(&client);
+    free_all_alloc();
+    return 0;
+}
