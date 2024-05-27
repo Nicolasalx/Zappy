277048daab62c883e59ce573d970f14e5d14f8cc
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-27 19:12:13 +0200

    add: new function to start the algo with the queen management in the ai part

---
+++ my_zappy_ai/src/queen_managment.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** queen_management
+*/
+
+#include "zappy_ai.h"
+
+void queen_management(client_t *client)
+{
+    push_new_command(client, INVENTORY, "Inventory\n");
+    if (client->inventory[LINEMATE] > 5) {
+        push_new_command(client, INCANTATION, "Incantation\n");
+    }
+}
