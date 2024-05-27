b24771d3fb18462e5cb5c1e0bd8526ec50754ece
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-27 18:10:33 +0200

    add: connect_nbr command for ia

---
+++ my_zappy_ai/src/command_reply/connect_nbr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** connect_nbr
+*/
+
+#include "zappy_ai.h"
+
+void connect_nbr_command(client_t *client, char *reply)
+{
+    printf("Connect_nbr\n");
+    client->last_cmd = CONNECT_NBR;
+    client->remaining_spots = atoi(reply);
+
+}
