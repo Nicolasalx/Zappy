3bdf633c7637c19a24b163aaadf8d98f8037b0fa
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-27 21:54:30 +0200

    add: set command for ia

---
+++ my_zappy_ai/src/command/set_cmd.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** set_cmd
+*/
+
+#include "zappy_ai.h"
+
+void set_command(client_t *client, char *ressource)
+{
+    char buffer[100] = {0};
+
+    printf("Set\n");
+    snprintf(buffer, sizeof(buffer), "Set %s\n", ressource);
+    for (int i = 0; i < NB_ITEM; ++i)
+        if (strcmp(ressource, object_list[i]) == 0) {
+            client->player.last_item_taken = i;
+            break;
+        }
+    push_new_command(client, SET, buffer);
+}
