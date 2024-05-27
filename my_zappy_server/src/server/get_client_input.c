60ba8f9ee7ec21f84dd57add04f2ca36fd2afe06
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-27 09:46:37 +0200

    add: buffering to the server input

--- my_zappy_server/src/server/get_client_input.c
+++ my_zappy_server/src/server/get_client_input.c
@@ -7,44 +7,55 @@
 
 #include "zappy_server.h"
 
-//static void buffering_input(client_t *client, char *command, ssize_t nb_byte)
-//{
-//    char *start_cmd = command;
-//    char *end_cmd = strchr(command, '\n');
-//
-//    while (end_cmd != NULL) {
-//        strncat(client->cmd_buffer, command, (end_cmd + 1) - start_cmd);
-//        execute_command(client, client->cmd_buffer);
-//        if (client->cmd_buffer == NULL) {
-//            return;
-//        }
-//        memset(client->cmd_buffer, 0, CMD_BUFFER_SIZE);
-//        memmove(start_cmd, end_cmd + 1, strlen(end_cmd + 1) + 1);
-//        end_cmd = strchr(command, '\n');
-//    }
-//    strncat(client->cmd_buffer, start_cmd, nb_byte);
-//}
+static void safe_strncat(char *dest, size_t *size_dest, const char *src, size_t n)
+{
+    size_t size_src = strlen(src);
+
+    if ((*size_dest + size_src) >= BUFFER_SIZE) {
+        dprintf(2, MAGENTA("[WARNING] internal buffer full.")"\n");
+        memset(dest, 0, BUFFER_SIZE);
+        *size_dest = 0;
+        return;
+    }
+    strncat(dest, src, n);
+    *size_dest += size_src;
+}
+
+static void buffering_input(server_t *server, client_t *client, char *command, size_t nb_byte)
+{
+    char *start_cmd = command;
+    char *end_cmd = strchr(command, '\n');
+
+    while (end_cmd != NULL) {
+        safe_strncat(client->cmd_buffer, &client->buffer_size, command, (end_cmd + 1) - start_cmd);
+        handle_client_input(server, client, client->cmd_buffer);
+
+        memset(client->cmd_buffer, 0, BUFFER_SIZE);
+        client->buffer_size = 0;
+        memmove(start_cmd, end_cmd + 1, strlen(end_cmd + 1) + 1);
+        end_cmd = strchr(command, '\n');
+    }
+    safe_strncat(client->cmd_buffer, &client->buffer_size, start_cmd, nb_byte);
+}
 
 void get_client_input(server_t *server, client_t *client)
 {
-    char cmd_data[BUFFER_SIZE] = {0};
+    char cmd_data[BUFFER_SIZE + 1] = {0};
     ssize_t nb_byte = read(client->fd, &cmd_data, sizeof(cmd_data));
 
-    if (nb_byte == -1)
+    if (nb_byte < 0)
         return;
     if (nb_byte == 0) {
         printf(YELLOW("A client has left")"\n");
-        if (client->player.id != -1)
-            pdi_reply(server, client);
+        pdi_reply(server, client);
         remove_client(client);
         return;
     }
     if (nb_byte >= BUFFER_SIZE) {
-        dprintf(2, RED("Client input is incompatible with the server.")"\n");
-        if (client->player.id != -1)
-            pdi_reply(server, client);
-        remove_client(client);
+        dprintf(2, MAGENTA("[WARNING] too long command.")"\n");
+        memset(client->cmd_buffer, 0, BUFFER_SIZE);
+        client->buffer_size = 0;
         return;
     }
-    handle_client_input(server, client, cmd_data);
+    buffering_input(server, client, cmd_data, nb_byte);
 }
