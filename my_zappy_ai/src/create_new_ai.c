77094efd8460a7ef0ab4f58aae06663ca34102a4
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-27 18:52:53 +0200

    add: fix compilation error ai zappy

--- my_zappy_ai/src/create_new_ai.c
+++ my_zappy_ai/src/create_new_ai.c
@@ -22,7 +22,7 @@ void create_new_ai(int port, struct in_addr *address, char *team_name)
 
         client.port = port;
         client.server_address.sin_addr = *address;
-        strcpy(client.team_name, team_name);
+        strcpy(client.player.team_name, team_name);
         //check_arg_validity(argc, argv, &client);
         create_client(&client);
         launch_client(&client);
