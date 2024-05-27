06e18da47d99babbe9405c09326557fac6bee0ee
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-27 09:52:18 +0200

    add: player handler

---
+++ my_zappy_ai/src/reply_handler.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** reply_handler
+*/
+
+#include "zappy_ai.h"
+
+const reply_handler_t reply_handler[] =
+{
+    {"Forward\n", false, NULL},
+    {"Right\n", false, NULL},
+    {"Left\n", false, NULL},
+    {"Look\n", false, NULL},
+    {"Inventory\n", false, NULL},
+    {"Broadcast", true, NULL},
+    {"Connect_nbr\n", false, NULL},
+    {"Fork\n", false, NULL},
+    {"Eject\n", false, NULL},
+    {"Take", true, NULL},
+    {"Set", true, NULL},
+    {"Incantation\n", false, NULL},
+    {NULL, false, NULL}
+};
