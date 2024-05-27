0f382a3fd80bf0c42f3b7080a56e159fbe9d7c34
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-27 09:51:43 +0200

    add: launch client function

---
+++ my_zappy_ai/src/lauch_client.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** lauch_client
+*/
+
+#include "zappy_ai.h"
+#include <signal.h>
+
+void close_client(int)
+{
+    exit_client(0, NULL);
+}
+
+void lauch_client(client_t *client)
+{
+    int max_fd = 0;
+
+    signal(SIGINT, close_client);
+    while (true) {
+        init_client_set(client, &max_fd);
+        monitor_input(client, max_fd);
+        handle_new_message(client);
+    }
+}
