1a6dbeaf13a9188ca5cb4f2d47f4084451a900bf
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-05-28 17:15:42 +0200

    Add 3D drawing functions to the GUI

---
+++ my_zappy_gui/src/graphic/draw/draw_3d.cpp
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** graphic_draw
+*/
+
+#include "zappy_gui.hpp"
+
+void Graphic::draw_sky_box()
+{
+    ClearBackground(RAYWHITE);
+    rlDisableBackfaceCulling();
+    rlDisableDepthMask();
+        DrawModel(sky_box, (Vector3){0, 0, 0}, 1.0f, WHITE);
+    rlEnableBackfaceCulling();
+    rlEnableDepthMask();
+}
+
+void Graphic::draw_map()
+{
+    for (int i = 0; i < this->gameState->map_size.x; i++) {
+        for (int j = 0; j < this->gameState->map_size.y; j++) {
+            if (rayInfo.type == ISLAND && rayInfo.x == i && rayInfo.y == j) {
+                this->rayInfo.box.min = (Vector3){i * SCALE - 2.2f, -4.2f, j * SCALE - 2.2f};
+                this->rayInfo.box.max = (Vector3){i * SCALE + 2.2f, 0.2f, j * SCALE + 2.2f};
+                DrawBoundingBox(this->rayInfo.box, GREEN);
+                DrawModel(this->model_list[ISLAND], (Vector3){i * SCALE, -2.0f, j * SCALE}, 2.0f, GREEN);
+            }
+            else {
+                DrawModel(this->model_list[ISLAND], (Vector3){i * SCALE, -2.0f, j * SCALE}, 2.0f, WHITE);
+            }
+        }
+    }
+}
+
+void Graphic::draw_object()
+{
+    for (int i = 0; i < this->gameState->map_size.x; i++) {
+        for (int j = 0; j < this->gameState->map_size.y; j++) {
+            for (int k = 0; k < 7; k++) {
+                if (this->gameState->object_pos[j][i][k] != 0) {
+                    DrawModel(this->model_list[k], (Vector3){i * SCALE + object_padding[k][0], 0.5f, j * SCALE + object_padding[k][1]}, 0.4f, WHITE);
+                }
+            }
+        }
+    }
+}
