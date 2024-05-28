455172cb198db6061da9ad8be4971fc1428f916a
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-28 14:44:52 +0200

    mod: management queen in the ai part has been modified

--- my_zappy_ai/src/algo/queen_managment.c
+++ my_zappy_ai/src/algo/queen_managment.c
@@ -7,18 +7,7 @@
 
 #include "zappy_ai.h"
 
-
-void broad_cast_cmd(client_t *client)
-{
-    send_command_broad_cast(client, "Queen role taken");
-}
-
-bool ok = false;
-
 void queen_management(client_t *client)
 {
-    if (!ok) {
-        broad_cast_cmd(client);
-        ok = true;
-    }
+
 }
