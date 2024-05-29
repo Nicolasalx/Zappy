a17b43734244ee3919bcb619d87266b4cc3c41de
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-05-29 16:25:25 +0200

    Update player position and animation timing in animation.cpp

--- my_zappy_gui/src/graphic/animation.cpp
+++ my_zappy_gui/src/graphic/animation.cpp
@@ -9,43 +9,27 @@
 
 void Graphic::update_player_pos(player_t &player)
 {
-    if (frame_time > 0.1f) {
-        player.real_pos.x = player.pos.x;
-        player.real_pos.y = player.pos.y;
-        return;
-    }
-    if (player.real_pos.x < player.pos.x - 2) {
-        player.real_pos.x = player.pos.x;
-    }
-    if (player.real_pos.x > player.pos.x + 2) {
-        player.real_pos.x = player.pos.x;
-    }
-    if (player.real_pos.y < player.pos.y - 2) {
-        player.real_pos.y = player.pos.y;
-    }
-    if (player.real_pos.y > player.pos.y + 2) {
-        player.real_pos.y = player.pos.y;
-    }
     if (player.real_pos.x < player.pos.x) {
-        player.real_pos.x += 0.05f;
+        player.real_pos.x += this->frame_time * 2;
     }
     if (player.real_pos.x > player.pos.x) {
-        player.real_pos.x -= 0.05f;
+        player.real_pos.x -= this->frame_time * 2;
     }
     if (player.real_pos.y < player.pos.y) {
-        player.real_pos.y += 0.05f;
+        player.real_pos.y += this->frame_time * 2;
     }
     if (player.real_pos.y > player.pos.y) {
-        player.real_pos.y -= 0.05f;
+        player.real_pos.y -= this->frame_time * 2;
     }
 }
 
 void Graphic::update_animation(player_t &player)
 {
-    if (frame_time > 0.1f) {
+    if (frame_time > 0.25f) {
         player.anim_frame_counter = 32;
         player.real_orientation = player_orientation[player.orientation];
-        this->update_player_pos(player);
+        player.real_pos.x = player.pos.x;
+        player.real_pos.y = player.pos.y;
         return;
     }
     if (player.anim_frame_counter > 31 && player.anim_frame_counter < 151) {
@@ -63,10 +47,10 @@ void Graphic::update_animation(player_t &player)
         player.anim_frame_counter = 32;
     }
     if (player_orientation[player.orientation] > player.real_orientation) {
-        player.real_orientation += 2;
+        player.real_orientation += frame_time * 200;
     }
     if (player_orientation[player.orientation] < player.real_orientation) {
-        player.real_orientation -= 2;
+        player.real_orientation -= frame_time * 200;
     }
     this->update_player_pos(player);
 }
