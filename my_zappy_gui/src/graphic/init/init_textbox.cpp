071874986b44925c40d7bf79d4bd28e80dfdadd9
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-05-29 16:03:48 +0200

    Add resize and add_text functions to TextBox class

--- my_zappy_gui/src/graphic/init/init_textbox.cpp
+++ my_zappy_gui/src/graphic/init/init_textbox.cpp
@@ -15,11 +15,20 @@ TextBox::TextBox(float x, float y, float width, float height, int window_width, 
     this->_height = height;
     _box = {window_width * _x, window_height * _y, window_width * _width, window_height * _height};
     _color = {200, 200, 255, 100};
+    this->_window_width = window_width;
+    this->_window_height = window_height;
 }
 
 void TextBox::resize(int window_width, int window_height)
 {
     _box = {window_width * _x, window_height * _y, window_width * _width, window_height * _height};
+    this->_window_width = window_width;
+    this->_window_height = window_height;
+}
+
+void TextBox::add_text(std::string text, float ypos)
+{
+    DrawText(text.c_str(), _box.x + _window_width * 0.03f, _box.y + this->_box.height * ypos, _window_width / 60, WHITE);
 }
 
 void TextBox::draw()
