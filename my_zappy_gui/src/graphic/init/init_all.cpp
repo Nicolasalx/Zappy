1e4172abea0d6d909277c84af7e4cb3e517182ff
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-05-29 16:03:39 +0200

    Remove commented code and update texture in init_island() function

--- my_zappy_gui/src/graphic/init/init_all.cpp
+++ my_zappy_gui/src/graphic/init/init_all.cpp
@@ -49,8 +49,6 @@ void Graphic::init_sky_box()
 
 void Graphic::init_island()
 {
-    //avec un cube pour l'instant
-    
     Model island = LoadModel("assets/plateform.obj");
     Texture2D texture = LoadTexture("assets/plateform1.png");
     SetTextureFilter(texture, TEXTURE_FILTER_TRILINEAR);
