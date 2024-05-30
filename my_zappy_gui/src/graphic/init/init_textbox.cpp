fa40759fe8902e5d27aa32e895a55bc0b5bdd84c
Ulysse <ulysse.rousselet@epitech.eu>
2024-05-30 19:49:32 +0200

    Refactor TextBox resize() method to handle different button states

--- my_zappy_gui/src/graphic/init/init_textbox.cpp
+++ my_zappy_gui/src/graphic/init/init_textbox.cpp
@@ -43,37 +43,40 @@ void TextBox::resize(int window_width, int window_height)
             this->_button = {_box.x + _box.width  - (window_width + window_height) * 0.01f, _box.y, (window_width + window_height) * 0.01f, (window_width + window_height) * 0.01f};
         }
         if (state == LEFT_BUTTON) {
-            this->_button = {_box.x - window_width * 0.1f + (window_width + window_height) * 0.01f, _box.y, (window_width + window_height) * 0.01f, (window_width + window_height) * 0.01f};
+            this->_button = {_box.x, _box.y, (window_width + window_height) * 0.01f, (window_width + window_height) * 0.01f};
         }
     } else {
         if (state == CLOSED_RIGHT) {
-            std::cout << "closed right" << std::endl;
-            _box = {window_width * _x - window_width * _width, window_height * _y, window_width * _width, window_height * _height};
+            _box = {-window_width * _width + (window_width + window_height) * 0.01f, window_height * _y, window_width * _width, window_height * _height};
             this->_window_width = window_width;
             this->_window_height = window_height;
             this->_button = {_box.x + _box.width  - (window_width + window_height) * 0.01f, _box.y, (window_width + window_height) * 0.01f, (window_width + window_height) * 0.01f};
+        }
+        if (state == CLOSED_LEFT) {
+            _box = {window_width - (window_width + window_height) * 0.01f, window_height * _y, window_width * _width, window_height * _height};
+            this->_window_width = window_width;
+            this->_window_height = window_height;
+            this->_button = {_box.x, _box.y, (window_width + window_height) * 0.01f, (window_width + window_height) * 0.01f};
         }
     }
 }
 
 void TextBox::open_close()
 {
-    std::cout << GetMousePosition().x << std::endl;
     if (CheckCollisionPointRec(GetMousePosition(), _button)) {
-        std::cout << "button clicked" << std::endl;
         if (state == RIGHT_BUTTON) {
             state = CLOSED_RIGHT;
             resize(_window_width, _window_height);
         }
-        if (state == LEFT_BUTTON) {
+        else if (state == LEFT_BUTTON) {
             state = CLOSED_LEFT;
             resize(_window_width, _window_height);
         }
-        if (state == CLOSED_RIGHT) {
+        else if (state == CLOSED_RIGHT) {
             state = RIGHT_BUTTON;
             resize(_window_width, _window_height);
         }
-        if (state == CLOSED_LEFT) {
+        else if (state == CLOSED_LEFT) {
             state = LEFT_BUTTON;
             resize(_window_width, _window_height);
         }
@@ -82,23 +85,30 @@ void TextBox::open_close()
 
 void TextBox::add_text(std::string text, float ypos)
 {
-    DrawText(text.c_str(), _box.x + _window_width * 0.03f, _box.y + this->_box.height * ypos, _window_width / 60, WHITE);
+    DrawText(text.c_str(), _box.x + _window_width * 0.025f, _box.y + this->_box.height * ypos, _window_width / 60, WHITE);
 }
 
 void TextBox::draw()
 {
     DrawRectangleRec(this->_box, _color);
     DrawRectangleLinesEx(this->_box, 1, WHITE);
-    if (state == RIGHT_BUTTON || state == LEFT_BUTTON) {
+    if (state != NO_BUTTON) {
         DrawRectangleRec(this->_button, _color);
         DrawRectangleLinesEx(this->_button, 1, WHITE);
     }
 }
 
+bool TextBox::is_closed()
+{
+    if (state == CLOSED_RIGHT || state == CLOSED_LEFT)
+        return true;
+    return false;
+}
+
 void Graphic::init_2D()
 {
-    this->textBoxs.push_back(TextBox(0.78f, 0.04f, 0.2f, 0.1f, window_width, window_height));
-    this->textBoxs.push_back(TextBox(0.78f, 0.47f, 0.2f, 0.51f, window_width, window_height));
-    this->textBoxs.push_back(TextBox(0.78f, 0.47f, 0.2f, 0.40f, window_width, window_height));
+    this->textBoxs.push_back(TextBox(0.77f, 0.04f, 0.21f, 0.1f, window_width, window_height, LEFT_BUTTON));
+    this->textBoxs.push_back(TextBox(0.77f, 0.47f, 0.21f, 0.51f, window_width, window_height));
+    this->textBoxs.push_back(TextBox(0.77f, 0.47f, 0.21f, 0.40f, window_width, window_height));
     this->textBoxs.push_back(TextBox(0.01f, 0.07f, 0.2f, 0.40f, window_width, window_height, RIGHT_BUTTON));
 }
