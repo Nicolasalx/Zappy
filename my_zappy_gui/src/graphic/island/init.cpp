c255c536802d381caa3125a54d91bf727972c86a
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-31 18:54:54 +0200

    mod: system to handle the different island on the map has changed

--- my_zappy_gui/src/graphic/island/init.cpp
+++ my_zappy_gui/src/graphic/island/init.cpp
@@ -7,11 +7,12 @@
 
 #include "zappy_gui.hpp"
 
-void Gui::Graphic::init_island()
+Gui::Island::Island(std::vector<Model> &modelList)
 {
     Model island = LoadModel("assets/plateform.obj");
     Texture2D texture = LoadTexture("assets/plateform1.png");
+
     SetTextureFilter(texture, TEXTURE_FILTER_TRILINEAR);
     SetMaterialTexture(&island.materials[0], MATERIAL_MAP_DIFFUSE, texture);
-    this->model_list.push_back(island);
+    modelList.push_back(island);
 }
