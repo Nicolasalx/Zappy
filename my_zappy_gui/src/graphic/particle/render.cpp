20c3457a0f71c8e1c7b57e60a271b5e64b513cd0
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-31 17:28:43 +0200

    fix: merge conflict has been fixed with the particle of Ulysse

--- my_zappy_gui/src/graphic/particle/render.cpp
+++ my_zappy_gui/src/graphic/particle/render.cpp
@@ -12,6 +12,6 @@ void Gui::Graphic::draw_particle()
     // std::cout << particle_systems.size() << std::endl;
     for (auto &particle_system : this->particle_systems) {
         particle_system.update_particle();
-        particle_system.draw(this->particle_texture, this->camera);
+        particle_system.draw(this->particle_texture, this->camera._data);
     }
 }
