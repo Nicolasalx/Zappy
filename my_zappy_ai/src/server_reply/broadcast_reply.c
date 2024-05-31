321251ca00c07f5da4e548a17fcab042b86a96a7
Guillaume <guillaume.dubuis@epitech.eu>
2024-05-31 15:55:45 +0200

    add: fix coding style

--- my_zappy_ai/src/server_reply/broadcast_reply.c
+++ my_zappy_ai/src/server_reply/broadcast_reply.c
@@ -7,11 +7,6 @@
 
 #include "zappy_ai.h"
 
-// void broad_cast_cmd(client_t *client)
-// {
-//     send_command_broad_cast(client, "Queen role taken");
-// }
-
 bool command_come_from_my_team(client_t *client, const char *reply)
 {
     int nb_word = count_nb_word(reply, "|");
@@ -32,14 +27,4 @@ void broadcast_reply(client_t *client, char *reply)
     if (!command_come_from_my_team(client, reply)) {
         return;
     }
-
-    printf("SAME TEAM\n");
-
-    //int nb_word = count_nb_word(reply, " \t\n");
-    //int *size_word = count_size_word(reply, " \t\n", nb_word);
-    //char **word = my_str_to_word(reply, " \t\n", nb_word, size_word);
-
-    //for (int i = 0; i < NB_ITEM; ++i) {
-    //    client->shared_inventory[i] += atoi(word[i + 2]); // pcq word[0] c'est "message" et word[1] c'est <orientation>,
-    //}
 }
