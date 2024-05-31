c914402bc98111de9cd1a24e22e5eb134dc66f86
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-31 11:48:29 +0200

    add: mutithreading handlieng in create client

--- my_zappy_ai/src/create_client.c
+++ my_zappy_ai/src/create_client.c
@@ -9,16 +9,19 @@
 
 void create_client(client_t *client)
 {
-    get_client(client);
     client->fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client->fd == -1) {
-        exit_client(84, RED("Fail to create the client socket\n"));
+        sem_post(&get_thread_list(NULL)->end_game);
+        exit_client(client, 84, RED("Fail to create the client socket\n"));
     }
     client->server_address.sin_family = AF_INET;
     client->server_address.sin_port = htons(client->port);
     if (connect(client->fd, (struct sockaddr *)
         &client->server_address, sizeof(client->server_address)) != 0) {
-        exit_client(84, RED("Connection to the server failed.\n"));
+        sem_post(&get_thread_list(NULL)->end_game);
+        exit_client(client, 84, RED("Connection to the server failed.\n"));
     }
+    pthread_mutex_lock(&get_thread_list(NULL)->mutex);
     client->reply_buffer = my_calloc(sizeof(char) * (CMD_BUFFER_SIZE + 1));
+    pthread_mutex_unlock(&get_thread_list(NULL)->mutex);
 }
