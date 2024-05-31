4760312604f6f560d3aeadce41acbd48eeaf80af
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-31 18:55:51 +0200

    mod: system to handle the player with the constructor has changed in the gui part

--- my_zappy_gui/src/graphic/player/init.cpp
+++ my_zappy_gui/src/graphic/player/init.cpp
@@ -6,14 +6,15 @@
 */
 
 #include "zappy_gui.hpp"
+#include "player.hpp"
 
-void Gui::Graphic::init_player()
+Gui::Player::Player(std::vector<Model> &modelList)
 {
     Model player = LoadModel("assets/Astronaut.iqm");
     Texture2D texture = LoadTexture("assets/AstronautColor.png");
     SetMaterialTexture(&player.materials[0], MATERIAL_MAP_DIFFUSE, texture);
     player.transform = MatrixRotateXYZ((Vector3){-90.0f * (PI / 180), 0.0f, 0.0f});
-    this->model_list.push_back(player);
+    modelList.push_back(player);
 
     int animsCount = 5;
     this->player_animation = LoadModelAnimations("assets/Astronaut.iqm", &animsCount);
