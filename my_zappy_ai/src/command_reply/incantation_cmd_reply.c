5ef180b12bb4a4e5c658e8dc53702c247f7d7c92
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-31 11:51:03 +0200

    add: mutithreading handlieng in cmd reply in incantation

--- my_zappy_ai/src/command_reply/incantation_cmd_reply.c
+++ my_zappy_ai/src/command_reply/incantation_cmd_reply.c
@@ -9,21 +9,19 @@
 
 void incantation_command_reply(client_t *client, char *reply)
 {
+    pthread_mutex_lock(&get_thread_list(NULL)->mutex);
     int nb_word = count_nb_word(reply, ":");
     int *size_word = count_size_word(reply, ":", nb_word);
     char **word = my_str_to_word(reply, ":", nb_word, size_word);
 
-    printf("NB WORD OF REPLY :[%d]:[%s]\n", nb_word, reply);
+    pthread_mutex_unlock(&get_thread_list(NULL)->mutex);
     if (strcmp(reply, "ko\n") == 0 || strcmp(reply, "Elevation underway\n") == 0) {
         return;
     }
     if (nb_word != 2) {
         return;
     }
-    printf("HERE RESULT: %s\n", word[0]);
     if (strcmp(word[0], "Current level") == 0) {
         client->level_player = atoi(word[1]);
     }
-    printf("Incantation\n");
-    printf("NEW LEVEL: %d\n", client->level_player);
 }
