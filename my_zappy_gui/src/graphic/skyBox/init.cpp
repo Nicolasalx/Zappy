49a6703e5211d83136abe490f1aae8016c48fd76
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-31 18:56:16 +0200

    mod: system to handle the skyBox has changed in the gui part

--- my_zappy_gui/src/graphic/skyBox/init.cpp
+++ my_zappy_gui/src/graphic/skyBox/init.cpp
@@ -6,20 +6,21 @@
 */
 
 #include "zappy_gui.hpp"
+#include "skybox.hpp"
 
-void Gui::Graphic::init_sky_box()
+Gui::SkyBox::SkyBox()
 {
     Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
-    this->sky_box = LoadModelFromMesh(cube);
-    this->sky_box.materials[0].shader = LoadShader(TextFormat("assets/skybox.vs", 330),
+    this->model = LoadModelFromMesh(cube);
+    this->model.materials[0].shader = LoadShader(TextFormat("assets/skybox.vs", 330),
                                             TextFormat("assets/skybox.fs", 330));
     int environmentMap = MATERIAL_MAP_CUBEMAP;
     int doGamma = 0;
     int vflipped = 0;
-    SetShaderValue(sky_box.materials[0].shader, GetShaderLocation(sky_box.materials[0].shader, "environmentMap"), &environmentMap, SHADER_UNIFORM_INT);
-    SetShaderValue(sky_box.materials[0].shader, GetShaderLocation(sky_box.materials[0].shader, "doGamma"), &doGamma, SHADER_UNIFORM_INT);
-    SetShaderValue(sky_box.materials[0].shader, GetShaderLocation(sky_box.materials[0].shader, "vflipped"), &vflipped, SHADER_UNIFORM_INT);
+    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "environmentMap"), &environmentMap, SHADER_UNIFORM_INT);
+    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "doGamma"), &doGamma, SHADER_UNIFORM_INT);
+    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "vflipped"), &vflipped, SHADER_UNIFORM_INT);
     Image img = LoadImage("assets/skybox3.png");
-    sky_box.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
+    model.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
     UnloadImage(img);
 }
