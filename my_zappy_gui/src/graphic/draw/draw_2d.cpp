11bae03f205681b0f80e958f457ed59c210b8823
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-01 21:11:31 +0200

    mod: system to draw with the raylib has been changed in the gui part

--- my_zappy_gui/src/graphic/draw/draw_2d.cpp
+++ my_zappy_gui/src/graphic/draw/draw_2d.cpp
@@ -7,25 +7,25 @@
 
 #include "zappy_gui.hpp"
 
-void Gui::Graphic::draw_general_info()
+void Gui::Graphic::drawGeneralInfo()
 {
     textBoxs[0].draw();
-    textBoxs[0].add_text(TextFormat("Number of players: %d", this->_gameState->players_list.size()), 0.5f/2.5f);
-    textBoxs[0].add_text(TextFormat("Server frequency: %d", this->_gameState->time_unit), 1.5f/2.5f);
+    textBoxs[0].addText(TextFormat("Number of players: %d", this->_gameState->players_list.size()), 0.5f/2.5f);
+    textBoxs[0].addText(TextFormat("Server frequency: %d", this->_gameState->time_unit), 1.5f/2.5f);
     DrawFPS(10, 10);
 }
 
-void Gui::Graphic::draw_tile_info()
+void Gui::Graphic::drawTileInfo()
 {
     if (this->rayInfo.type == ISLAND) {
         textBoxs[2].draw();
-        textBoxs[2].add_text(TextFormat("Tile: (%d, %d)"), 0.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Food: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][FOOD]), 1.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Linemate: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][LINEMATE]), 2.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Deraumere: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][DERAUMERE]), 3.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Sibur: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][SIBUR]), 4.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Mendiane: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][MENDIANE]), 5.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Phiras: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][PHIRAS]), 6.5f/8.5f);
-        textBoxs[2].add_text(TextFormat("Thystame: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][THYSTAME]), 7.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Tile: (%d, %d)"), 0.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Food: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][FOOD]), 1.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Linemate: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][LINEMATE]), 2.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Deraumere: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][DERAUMERE]), 3.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Sibur: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][SIBUR]), 4.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Mendiane: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][MENDIANE]), 5.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Phiras: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][PHIRAS]), 6.5f/8.5f);
+        textBoxs[2].addText(TextFormat("Thystame: %d", this->_gameState->object_pos[this->rayInfo.y][this->rayInfo.x][THYSTAME]), 7.5f/8.5f);
     }
 }
