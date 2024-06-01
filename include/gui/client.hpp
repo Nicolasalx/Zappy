b57a46676453b25f733c5ba85036e16bb50ffd2c
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-01 17:20:37 +0200

    chore: Refactor Client class to accept shared_ptr<GameState> in constructor

--- include/gui/client.hpp
+++ include/gui/client.hpp
@@ -18,27 +18,32 @@
     #include <map>
     #include <iostream>
     #include "my_tracked_exception.hpp"
-
+    #include <memory>
     #include "gameState.hpp"
 
 namespace Gui
 {
+    constexpr int MAX_PORT_NB = 65535;
+    constexpr int BUFFER_SIZE = 4096;
+    constexpr int CMD_BUFFER_SIZE = 4096;
+
     class Client {
         public:
-            Client(int argc, const char **argv);
+            Client(int argc, const char **argv, std::shared_ptr<GameState> gameState);
             ~Client();
-            void create_client();
+
             void init_client_set();
-            void launch_client();
             void monitor_input();
             void handle_new_input();
             void handle_new_message();
             void get_args(int argc, const char **argv);
             void check_new_message(std::string reply_data);
             void send_cmd_to_server(char *cmd, int nb_byte);
-            void launch_graphic();
 
         private:
+            void create_client();
+            void launch_client();
+
             int fd = 0;
             int max_fd = 0;
             fd_set read_set;
@@ -47,8 +52,8 @@ namespace Gui
             struct sockaddr_in server_address;
             std::string cmd_buffer;
             std::string ip;
-            GameState gameState;
-            std::thread graphic_thread;
+            
+            std::shared_ptr<Gui::GameState> _gameState;
     };
 }
 
