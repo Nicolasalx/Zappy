7c3dea76cdbf45c7bf92e654a9c7d668f138cfb6
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-01 20:26:30 +0200

    refactor: Update Gui::Graphic to Gui::Player in animation.cpp

--- my_zappy_gui/src/graphic/animation.cpp
+++ my_zappy_gui/src/graphic/animation.cpp
@@ -7,50 +7,50 @@
 
 #include "zappy_gui.hpp"
 
-void Gui::Graphic::update_player_pos(player_t &player)
+void Gui::Player::update_player_pos(player_t &player)
 {
     if (player.real_pos.x < player.pos.x) {
-        player.real_pos.x += this->frame_time * 2;
+        player.real_pos.x += this->_gameState->frame_time * 2;
     }
     if (player.real_pos.x > player.pos.x) {
-        player.real_pos.x -= this->frame_time * 2;
+        player.real_pos.x -= this->_gameState->frame_time * 2;
     }
     if (player.real_pos.y < player.pos.y) {
-        player.real_pos.y += this->frame_time * 2;
+        player.real_pos.y += this->_gameState->frame_time * 2;
     }
     if (player.real_pos.y > player.pos.y) {
-        player.real_pos.y -= this->frame_time * 2;
+        player.real_pos.y -= this->_gameState->frame_time * 2;
     }
 }
 
-void Gui::Graphic::update_animation(player_t &player)
+void Gui::Player::update_animation(player_t &player)
 {
-    if (frame_time > 0.25f) {
+    if (_gameState->frame_time > 0.25f) {
         player.anim_frame_counter = 32;
-        player.real_orientation = this->player.player_orientation[player.orientation];
+        player.real_orientation = this->player_orientation[player.orientation];
         player.real_pos.x = player.pos.x;
         player.real_pos.y = player.pos.y;
         return;
     }
     if (player.anim_frame_counter > 31 && player.anim_frame_counter < 151) {
-        UpdateModelAnimation(this->_modelList[PLAYER], this->player.player_animation[player.animation_nbr], player.anim_frame_counter);
+        UpdateModelAnimation(this->player_model, this->player_animation[player.animation_nbr], player.anim_frame_counter);
         player.anim_frame_counter++;
     }
     if (player.anim_frame_counter == 151) {
         player.anim_frame_counter = 32;
     }
     if (player.anim_frame_counter > 151 && player.anim_frame_counter < 182) {
-        UpdateModelAnimation(this->_modelList[PLAYER], this->player.player_animation[player.animation_nbr], player.anim_frame_counter);
+        UpdateModelAnimation(this->player_model, this->player_animation[player.animation_nbr], player.anim_frame_counter);
         player.anim_frame_counter++;
     }
     if (player.anim_frame_counter == 182) {
         player.anim_frame_counter = 32;
     }
-    if (this->player.player_orientation[player.orientation] > player.real_orientation) {
-        player.real_orientation += frame_time * 200;
+    if (this->player_orientation[player.orientation] > player.real_orientation) {
+        player.real_orientation += _gameState->frame_time * 200;
     }
-    if (this->player.player_orientation[player.orientation] < player.real_orientation) {
-        player.real_orientation -= frame_time * 200;
+    if (this->player_orientation[player.orientation] < player.real_orientation) {
+        player.real_orientation -= _gameState->frame_time * 200;
     }
     this->update_player_pos(player);
 }
