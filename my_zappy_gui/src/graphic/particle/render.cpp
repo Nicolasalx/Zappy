460909aa2c1bf304960a0e9dce4922e28d19a1fc
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-01 20:27:40 +0200

    refactor: Update Gui::Player constructor to load player model and texture

--- my_zappy_gui/src/graphic/particle/render.cpp
+++ my_zappy_gui/src/graphic/particle/render.cpp
@@ -7,11 +7,11 @@
 
 #include "zappy_gui.hpp"
 
-void Gui::Graphic::draw_particle()
+void Gui::ParticleSystem::draw_particle(Camera3D camera)
 {
     // std::cout << particle_systems.size() << std::endl;
     for (auto &particle_system : this->particle_systems) {
-        particle_system.update_particle(this->frame_time);
-        particle_system.draw(this->particle_texture, this->camera._data);
+        particle_system.update_particle(this->_gameState->frame_time);
+        particle_system.draw(this->particle_texture, camera);
     }
 }
