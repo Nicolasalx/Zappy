3cc85fa23428df6585935eff2f5c675005743d13
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-01 17:21:09 +0200

    refactor: Update Object constructor to accept shared_ptr<GameState>

--- include/gui/object.hpp
+++ include/gui/object.hpp
@@ -10,19 +10,22 @@
 
     #include "raylibInfo.hpp"
     #include <vector>
-    //#include "zappy_gui.hpp"
+    #include <memory>
 
 namespace Gui
 {
     class Object
     {
         public:
-            Object(std::vector<Model> &modelList);
+            Object(std::vector<Model> &modelList, std::shared_ptr<GameState> gameState);
             ~Object() = default;
 
             float object_padding[7][2];
 
+            void render();
+
         private:
+            std::shared_ptr<GameState> _gameState;
 
     };
 }
