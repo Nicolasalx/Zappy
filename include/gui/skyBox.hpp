bfdd6f949ed20d9419dc15c2aa670172ee66dee6
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-01 17:21:30 +0200

    refactor: Add SkyBox class with shared_ptr<GameState> parameter in constructor

---
+++ include/gui/skyBox.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** skyBox
+*/
+
+#ifndef SKYBOX_H_
+    #define SKYBOX_H_
+
+    #include "infoGame.hpp"
+
+namespace Gui
+{
+    class SkyBox {
+        public:
+            SkyBox(std::shared_ptr<GameState> gameState);
+            ~SkyBox() = default;
+
+            Model model;
+
+        private:
+            std::shared_ptr<GameState> _gameState;
+
+    };
+}
+
+#endif /* !SKYBOX_H_ */
