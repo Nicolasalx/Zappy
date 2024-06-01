8abbd75094d7d9f0cc3ae82803ac8c865094b252
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-01 21:12:39 +0200

    mod: system to handle a new player has been changed in the gui part

--- my_zappy_gui/src/graphic/player/renderPlayerInfo.cpp
+++ my_zappy_gui/src/graphic/player/renderPlayerInfo.cpp
@@ -7,33 +7,33 @@
 
 #include "zappy_gui.hpp"
 
-void Gui::Graphic::draw_player_info()
+void Gui::Graphic::drawPlayerInfo()
 {
     if (this->rayInfo.type == PLAYER) {
         for (auto &player : this->_gameState->players_list) {
             if (player.n == this->rayInfo.id) {
                 textBoxs[1].draw();
-                textBoxs[1].add_text(TextFormat("Player: %d", player.n), 0.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Level: %d", player.level), 1.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Orientation: %d", player.orientation), 2.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Position: (%.2f, %.2f)", player.real_pos.x, player.real_pos.y), 3.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Inventory:"), 4.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Food: %d", player.inventory[FOOD]), 5.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Linemate: %d", player.inventory[LINEMATE]), 6.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Deraumere: %d", player.inventory[DERAUMERE]), 7.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Sibur: %d", player.inventory[SIBUR]), 8.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Mendiane: %d", player.inventory[MENDIANE]), 9.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Phiras: %d", player.inventory[PHIRAS]), 10.5f/12.5f);
-                textBoxs[1].add_text(TextFormat("Thystame: %d", player.inventory[THYSTAME]), 11.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Player: %d", player.n), 0.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Level: %d", player.level), 1.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Orientation: %d", player.orientation), 2.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Position: (%.2f, %.2f)", player.real_pos.x, player.real_pos.y), 3.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Inventory:"), 4.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Food: %d", player.inventory[FOOD]), 5.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Linemate: %d", player.inventory[LINEMATE]), 6.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Deraumere: %d", player.inventory[DERAUMERE]), 7.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Sibur: %d", player.inventory[SIBUR]), 8.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Mendiane: %d", player.inventory[MENDIANE]), 9.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Phiras: %d", player.inventory[PHIRAS]), 10.5f/12.5f);
+                textBoxs[1].addText(TextFormat("Thystame: %d", player.inventory[THYSTAME]), 11.5f/12.5f);
             }
         }
     }
 }
 
-void Gui::Graphic::draw_all_player_level()
+void Gui::Graphic::drawAllPlayerLevel()
 {
     textBoxs[3].draw();
-    if (textBoxs[3].is_closed())
+    if (textBoxs[3].isClosed())
         return;
     for (int i = 0; i < 8; ++i) {
         this->listLevelPlayer.at(i) = 0;
@@ -42,6 +42,6 @@ void Gui::Graphic::draw_all_player_level()
         this->listLevelPlayer.at(player.level - 1) += 1;
     }
     for (int i = 0; i < 8; ++i) {
-        textBoxs[3].add_text(TextFormat("Level %d: %d", (i + 1), listLevelPlayer[i]), (i + 0.5f)/8.5f);
+        textBoxs[3].addText(TextFormat("Level %d: %d", (i + 1), listLevelPlayer[i]), (i + 0.5f)/8.5f);
     }
 }
