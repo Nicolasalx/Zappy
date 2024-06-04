484105d2912f3bb21c21b495878dfcf7bcd7b941
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-04 16:25:20 +0200

    feat: Update rayInfo box size in render.cpp

--- my_zappy_gui/src/graphic/island/render.cpp
+++ my_zappy_gui/src/graphic/island/render.cpp
@@ -12,13 +12,13 @@ void Gui::Island::drawMap(ray_info_t &rayInfo)
     for (int i = 0; i < this->_gameState->map_size.x; i++) {
         for (int j = 0; j < this->_gameState->map_size.y; j++) {
             if (rayInfo.type == ISLAND && rayInfo.x == i && rayInfo.y == j) {
-                rayInfo.box.min = (Vector3){i * Gui::MAP_SCALE - 2.2f, -4.2f, j * Gui::MAP_SCALE - 2.2f};
-                rayInfo.box.max = (Vector3){i * Gui::MAP_SCALE + 2.2f, 0.2f, j * Gui::MAP_SCALE + 2.2f};
+                rayInfo.box.min = (Vector3){i * Gui::MAP_SCALE - 2.2f * this->size, -4.2f * this->size, j * Gui::MAP_SCALE - 2.2f * this->size};
+                rayInfo.box.max = (Vector3){i * Gui::MAP_SCALE + 2.2f * this->size, 0.2f * this->size, j * Gui::MAP_SCALE + 2.2f * this->size};
                 DrawBoundingBox(rayInfo.box, GREEN);
-                DrawModel(this->island_model, (Vector3){i * Gui::MAP_SCALE, -2.0f, j * Gui::MAP_SCALE}, 2.0f, GREEN);
+                DrawModel(this->island_model, (Vector3){i * Gui::MAP_SCALE, -2.0f * this->size, j * Gui::MAP_SCALE}, 2.0f * this->size, GREEN);
             }
             else {
-                DrawModel(this->island_model, (Vector3){i * Gui::MAP_SCALE, -2.0f, j * Gui::MAP_SCALE}, 2.0f, WHITE);
+                DrawModel(this->island_model, (Vector3){i * Gui::MAP_SCALE, -2.0f * this->size, j * Gui::MAP_SCALE}, 2.0f * this->size, WHITE);
             }
         }
     }
