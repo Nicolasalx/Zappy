207028858c7b7c30cf841a9c87d0d4c1503d67d6
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:40:16 +0200

    fix: update server frequency option

--- my_zappy_server/src/args_management/get_server_info.c
+++ my_zappy_server/src/args_management/get_server_info.c
@@ -29,7 +29,7 @@ static void get_frequency(const char **argv, server_t *server, int i)
         if (!my_str_only_cont(argv[i + 1], "0123456789")) {
             my_error("Error: Frequency must be a number", 84);
         }
-        server->freq = atoi(argv[i + 1]);
+        server->opt.freq = atoi(argv[i + 1]);
     }
 }
 
