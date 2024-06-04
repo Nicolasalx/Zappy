4f5167d4c69c4d892f36dff7bf8ca1a9df5d6075
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:41:11 +0200

    fix: incorrect variable reference in pin_cmd.c

--- my_zappy_server/src/gui_command/pin_cmd.c
+++ my_zappy_server/src/gui_command/pin_cmd.c
@@ -11,7 +11,7 @@ static void print_inventory(server_t *server, client_t *client, int i)
 {
     char buffer[100] = {0};
 
-    if (server->clients[i].fd != 0 && server->clients[i].is_graphic == false
+    if (server->clients[i].fd != 0 && server->clients[i].player.is_graphic == false
     && server->clients[i].player.team) {
         snprintf(buffer, sizeof(buffer),
             "pin %d %d %d %d %d %d %d %d %d %d\n",
@@ -39,7 +39,7 @@ void pin_cmd(int, char **argv, client_t *client, server_t *server)
     }
     id = atoi(argv[0]);
     for (int i = 0; i < MAX_CLIENT; i++) {
-        if (server->clients[i].fd != 0 && server->clients[i].is_graphic
+        if (server->clients[i].fd != 0 && server->clients[i].player.is_graphic
         == false && server->clients[i].player.id == id) {
             print_inventory(server, client, i);
             return;
