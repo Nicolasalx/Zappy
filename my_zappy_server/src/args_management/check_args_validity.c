d842e68c879f20fe3904edd0fb07f89d28afcceb
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:39:42 +0200

    refactor: server struct in check_args_validity.c

--- my_zappy_server/src/args_management/check_args_validity.c
+++ my_zappy_server/src/args_management/check_args_validity.c
@@ -9,55 +9,55 @@
 
 static void create_team_name(server_t *server)
 {
-    if (server->team_count == 0) {
+    if (server->game.team_count == 0) {
         printf("No team name set, default team names are "
             "Team1, Team2, Team3, Team4\n");
-        strcpy(server->team_list[0].name, "Team1");
-        server->team_list[0].remaining_spot = server->client_nb;
-        strcpy(server->team_list[1].name, "Team2");
-        server->team_list[1].remaining_spot = server->client_nb;
-        strcpy(server->team_list[2].name, "Team3");
-        server->team_list[2].remaining_spot = server->client_nb;
-        strcpy(server->team_list[3].name, "Team4");
-        server->team_list[3].remaining_spot = server->client_nb;
-        server->team_count = 4;
+        strcpy(server->game.team_list[0].name, "Team1");
+        server->game.team_list[0].remaining_spot = server->opt.client_nb;
+        strcpy(server->game.team_list[1].name, "Team2");
+        server->game.team_list[1].remaining_spot = server->opt.client_nb;
+        strcpy(server->game.team_list[2].name, "Team3");
+        server->game.team_list[2].remaining_spot = server->opt.client_nb;
+        strcpy(server->game.team_list[3].name, "Team4");
+        server->game.team_list[3].remaining_spot = server->opt.client_nb;
+        server->game.team_count = 4;
     }
 }
 
 static void fill_arg(server_t *server)
 {
-    if (server->world.size_x == -1 && server->world.size_y == -1) {
+    if (server->game.world.size_x == -1 && server->game.world.size_y == -1) {
         printf("No map dimensions set, default is 10x10\n");
-        server->world.size_x = 10;
-        server->world.size_y = 10;
+        server->game.world.size_x = 10;
+        server->game.world.size_y = 10;
     }
-    if (server->world.size_x == -1 || server->world.size_y == -1)
+    if (server->game.world.size_x == -1 || server->game.world.size_y == -1)
         my_error("Error: Map dimensions are not set", 84);
-    if (server->client_nb == -1) {
+    if (server->opt.client_nb == -1) {
         printf("No client number set, default client number is 4\n");
-        server->client_nb = 4;
+        server->opt.client_nb = 4;
     }
     create_team_name(server);
     if (server->port == -1) {
         printf("No port set, default port is 4242\n");
         server->port = 4242;
     }
-    if (server->freq == -1) {
+    if (server->opt.freq == -1) {
         printf("No frequency set, default frequency is 100 tps\n");
-        server->freq = 100;
+        server->opt.freq = 100;
     }
 }
 
 static void check_server_arg(server_t *server)
 {
-    if ((server->client_nb <= 0 || server->client_nb > MAX_CLIENT_NB)
-    && server->client_nb != -1) {
+    if ((server->opt.client_nb <= 0 || server->opt.client_nb > MAX_CLIENT_NB)
+    && server->opt.client_nb != -1) {
         my_error("Error: Clients must be between 1 and 100", 84);
     }
-    if ((server->freq <= 0 || server->freq > 150) && server->freq != -1) {
+    if ((server->opt.freq <= 0 || server->opt.freq > 150) && server->opt.freq != -1) {
         my_error("Error: Frequency cannot be greater than 150", 84);
     }
-    if (server->team_count > MAX_TEAM_NB) {
+    if (server->game.team_count > MAX_TEAM_NB) {
         my_error("Error: You can't have more than 10 teams", 84);
     }
 }
@@ -68,12 +68,12 @@ void check_arg_validity(server_t *server)
     server->port != -1) {
         my_error("Error: Port must be between 1 and 65535", 84);
     }
-    if ((server->world.size_x <= 0 || server->world.size_y > 100)
-    && server->world.size_x != -1) {
+    if ((server->game.world.size_x <= 0 || server->game.world.size_y > 100)
+    && server->game.world.size_x != -1) {
         my_error("Error: Width must be between 1 and 100", 84);
     }
-    if ((server->world.size_y <= 0 || server->world.size_y > 100)
-    && server->world.size_y != -1) {
+    if ((server->game.world.size_y <= 0 || server->game.world.size_y > 100)
+    && server->game.world.size_y != -1) {
         my_error("Error: Height must be between 1 and 100", 84);
     }
     check_server_arg(server);
