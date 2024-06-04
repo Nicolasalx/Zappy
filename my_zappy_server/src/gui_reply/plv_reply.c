e14756891ba5d4a3aef1dc21f1fc0bbc0874bdc1
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:43:17 +0200

    fix: is_graphic condition in plv_reply function

--- my_zappy_server/src/gui_reply/plv_reply.c
+++ my_zappy_server/src/gui_reply/plv_reply.c
@@ -15,7 +15,7 @@ void plv_reply(server_t *server, client_t *client)
     client->player.id, client->player.level);
     for (int i = 0; i < MAX_CLIENT; i++) {
         if (server->clients[i].fd != 0
-        && server->clients[i].is_graphic == true) {
+        && server->clients[i].player.is_graphic == true) {
             send_msg_client(server->clients[i].fd, buffer);
         }
     }
