d13e8fadda8e7d4df1c6a243d2c786ad3c1ed72f
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:40:23 +0200

    refactor: team management in get_teams.c

--- my_zappy_server/src/args_management/get_teams.c
+++ my_zappy_server/src/args_management/get_teams.c
@@ -16,14 +16,14 @@ void get_clients_nb(const char **argv, server_t *server, int i)
         if (!my_str_only_cont(argv[i + 1], "0123456789")) {
             my_error("Error: Clients must be a number", 84);
         }
-        server->client_nb = atoi(argv[i + 1]);
+        server->opt.client_nb = atoi(argv[i + 1]);
     }
 }
 
-static bool is_team_name_taken(const char *name, server_t *server)
+static bool is_team_name_taken(const char *name, game_t *game)
 {
-    for (int j = 0; j < server->team_count; j++) {
-        if (strcmp(server->team_list[j].name, name) == 0) {
+    for (int j = 0; j < game->team_count; j++) {
+        if (strcmp(game->team_list[j].name, name) == 0) {
             return true;
         }
     }
@@ -45,7 +45,7 @@ static void get_teams_name_error(const char **argv, server_t *server, int i)
     if (strlen(argv[i]) > MAX_TEAMNAME_SIZE) {
         my_error("Error: Team name is too long", 84);
     }
-    if (is_team_name_taken(argv[i], server)) {
+    if (is_team_name_taken(argv[i], &server->game)) {
         my_error("Error: Team name already taken", 84);
     }
     if (strcmp(argv[i], "GRAPHIC") == 0) {
@@ -56,21 +56,20 @@ static void get_teams_name_error(const char **argv, server_t *server, int i)
     }
 }
 
-void get_teams_name(const char **argv,
-    server_t *server, int i, int argc)
+void get_teams_name(const char **argv, server_t *server, int i, int argc)
 {
     if (strcmp(argv[i], "-n") == 0) {
         for (i = i + 1; i < argc && argv[i][0] != '-'; i++) {
             get_teams_name_error(argv, server, i);
-            strcpy(server->team_list[server->team_count].name, argv[i]);
-            server->team_list[server->team_count].
-            remaining_spot = server->client_nb;
-            server->team_list[server->team_count].
+            strcpy(server->game.team_list[server->game.team_count].name, argv[i]);
+            server->game.team_list[server->game.team_count].
+            remaining_spot = server->opt.client_nb;
+            server->game.team_list[server->game.team_count].
             player_max = 0;
-            server->team_count += 1;
+            server->game.team_count += 1;
         }
-        if (server->team_count == 0 ||
-            !strcmp(server->team_list[0].name, "")) {
+        if (server->game.team_count == 0 ||
+            !strcmp(server->game.team_list[0].name, "")) {
             my_error("Error: No team name specified", 84);
         }
     }
