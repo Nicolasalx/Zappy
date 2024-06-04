0b7ddd221503a17415ddd18be203fbdba3c95826
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-04 16:25:01 +0200

    feat: Adjust position and scale of eggs in render.cpp

--- my_zappy_gui/src/graphic/egg/render.cpp
+++ my_zappy_gui/src/graphic/egg/render.cpp
@@ -11,6 +11,6 @@
 void Gui::Egg::draw()
 {
     for (auto it = this->_gameState->eggs_list.begin(); it != this->_gameState->eggs_list.end(); it++) {
-        DrawModel(this->_egg_model, (Vector3){it->pos.x * Gui::MAP_SCALE, 0.0f, it->pos.y * Gui::MAP_SCALE}, 1.0f, WHITE);
+        DrawModel(this->_egg_model, (Vector3){it->pos.x * Gui::MAP_SCALE - 1.3f, 0.9f, it->pos.y * Gui::MAP_SCALE - 1.3f}, 0.9f, WHITE);
     }
 }
