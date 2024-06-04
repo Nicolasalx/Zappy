a90398c82b7caeabe6ae1dffbdc465c969b1841a
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:41:33 +0200

    fix: sgt_cmd to use server->opt.freq

--- my_zappy_server/src/gui_command/sgt_cmd.c
+++ my_zappy_server/src/gui_command/sgt_cmd.c
@@ -11,6 +11,6 @@ void sgt_cmd(int, char **, client_t *client, server_t *server)
 {
     char buffer[100] = {0};
 
-    snprintf(buffer, sizeof(buffer), "sgt %d\n", (int)server->freq);
+    snprintf(buffer, sizeof(buffer), "sgt %d\n", (int)server->opt.freq);
     send_msg_client(client->fd, buffer);
 }
