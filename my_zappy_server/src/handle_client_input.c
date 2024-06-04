309545fdcffc1f61d0ba2a9ae4f9178980f2b235
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:35:16 +0200

    refactor: handle_client_input.c

--- my_zappy_server/src/handle_client_input.c
+++ my_zappy_server/src/handle_client_input.c
@@ -10,20 +10,20 @@
 static void send_egg_info(char *buffer,
     node_t *current, int i, server_t *server)
 {
-    if (server->team_list[i].egg_list == NULL) {
+    if (server->game.team_list[i].egg_list == NULL) {
         return;
     }
     do {
         snprintf(buffer, 100, "%d %d %d %s",
         GET_DATA(current, egg_t)->nb, GET_DATA(current, egg_t)->pos_x,
-        GET_DATA(current, egg_t)->pos_y, server->team_list[i].name);
+        GET_DATA(current, egg_t)->pos_y, server->game.team_list[i].name);
         smg_reply(server, buffer);
         memset(buffer, 0, 100);
-        if (server->team_list[i].egg_list == NULL) {
+        if (server->game.team_list[i].egg_list == NULL) {
             return;
         }
         current = current->next;
-    } while (current != server->team_list[i].egg_list);
+    } while (current != server->game.team_list[i].egg_list);
 }
 
 static void eggs_info(server_t *server)
@@ -31,8 +31,8 @@ static void eggs_info(server_t *server)
     char buffer[100] = {0};
     node_t *current = NULL;
 
-    for (int i = 0; i < server->team_count; ++i) {
-        current = server->team_list[i].egg_list;
+    for (int i = 0; i < server->game.team_count; ++i) {
+        current = server->game.team_list[i].egg_list;
         send_egg_info(buffer, current, i, server);
     }
 }
@@ -41,7 +41,7 @@ static void handle_new_graphic_client(server_t *server, client_t *client)
 {
     char buffer[100] = {0};
 
-    client->is_graphic = true;
+    client->player.is_graphic = true;
     msz_cmd(0, NULL, client, server);
     sgt_cmd(0, NULL, client, server);
     mct_cmd(0, NULL, client, server);
@@ -66,11 +66,11 @@ static void handle_new_player_client(
 {
     char buff[MAX_TEAMNAME_SIZE + 2] = {0};
 
-    for (int i = 0; i < server->team_count; ++i) {
-        strcpy(buff, server->team_list[i].name);
+    for (int i = 0; i < server->game.team_count; ++i) {
+        strcpy(buff, server->game.team_list[i].name);
         strcat(buff, "\n");
         if (strcmp(cmd, buff) == 0) {
-            client->player.team = &server->team_list[i];
+            client->player.team = &server->game.team_list[i];
             init_player(client, server);
             return;
         }
@@ -90,15 +90,15 @@ static void dispatch_client_input(
 
 void handle_client_input(server_t *server, client_t *client, char *cmd)
 {
-    if (server->end_game) {
+    if (server->opt.end_game) {
         send_msg_client(client->fd, "End of Game\n");
         return;
     }
-    if (server->pause_game) {
+    if (server->opt.pause_game) {
         send_msg_client(client->fd, "Game is paused\n");
         return;
     }
-    if (client->is_graphic) {
+    if (client->player.is_graphic) {
         handle_gui_input(server, client, cmd);
     } else if (client->player.team) {
         handle_ai_input(server, client, cmd);
