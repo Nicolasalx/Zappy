af5b06a0106fe2dafaa4eca18ae3d89f856127dc
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-04 18:35:48 +0200

    refactor: spawn_resource function to use game_t struct

--- my_zappy_server/src/spawn_resource.c
+++ my_zappy_server/src/spawn_resource.c
@@ -7,11 +7,11 @@
 
 #include "zappy_server.h"
 
-static void count_ressource(server_t *server, int i, int count_spawn[NB_ITEM])
+static void count_ressource(game_t *game, int i, int count_spawn[NB_ITEM])
 {
-    for (int j = 0; j < server->world.size_y; ++j) {
-        for (int k = 0; k < server->world.size_x; ++k) {
-            count_spawn[i] += server->world.map[j][k].item[i];
+    for (int j = 0; j < game->world.size_y; ++j) {
+        for (int k = 0; k < game->world.size_x; ++k) {
+            count_spawn[i] += game->world.map[j][k].item[i];
         }
     }
 }
@@ -24,28 +24,28 @@ static void add_new_ressource(server_t *server, int i,
 
     if (nb_spawn[i] - count_spawn[i] > 0) {
         for (int j = 0; j < nb_spawn[i] - count_spawn[i]; ++j) {
-            random_x = rand() % server->world.size_x;
-            random_y = rand() % server->world.size_y;
-            server->world.map[random_y][random_x].item[i] += 1;
+            random_x = rand() % server->game.world.size_x;
+            random_y = rand() % server->game.world.size_y;
+            server->game.world.map[random_y][random_x].item[i] += 1;
             bct_reply(server, random_x, random_y);
         }
     }
 }
 
-void spawn_resource(server_t *server)
+void spawn_resource(server_t *server, game_t *game)
 {
     int count_spawn[NB_ITEM] = {0};
     int nb_spawn[NB_ITEM] = {0};
 
     for (int i = 0; i < NB_ITEM; ++i) {
-        nb_spawn[i] = round((server->world.size_x *
-        server->world.size_y) * resource_density[i]);
+        nb_spawn[i] = round((game->world.size_x *
+        game->world.size_y) * resource_density[i]);
         if (nb_spawn[i] <= 0) {
             nb_spawn[i] = 1;
         }
     }
     for (int i = 0; i < NB_ITEM; ++i) {
-        count_ressource(server, i, count_spawn);
+        count_ressource(game, i, count_spawn);
     }
     for (int i = 0; i < NB_ITEM; ++i) {
         add_new_ressource(server, i, count_spawn, nb_spawn);
