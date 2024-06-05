76948fc3cfc09c4e1f3f3db61bd3e1268cd66ec1
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-05 17:19:42 +0200

    fix: ai coding style

--- my_zappy_ai/src/exit_client.c
+++ my_zappy_ai/src/exit_client.c
@@ -9,10 +9,13 @@
 
 void exit_client(client_t *client, int exit_value, const char *message)
 {
-    (void) exit_value;
-
     if (message != NULL) {
         my_putstr(message);
+    }
+    if (exit_value) {
+        pthread_mutex_lock(&get_thread_list(NULL)->mutex);
+        get_thread_list(NULL)->exit_code = exit_value;
+        pthread_mutex_unlock(&get_thread_list(NULL)->mutex);
     }
     delete_client(client);
     remove_thread_from_list();
