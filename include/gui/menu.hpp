3899ac29cac662ede5e460c6e0a21d3dd75a1836
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-05 11:07:40 +0200

    mod: header menu has new private member to handle all the menu in the gui part

--- include/gui/menu.hpp
+++ include/gui/menu.hpp
@@ -14,16 +14,70 @@
 
 namespace Gui
 {
+    enum ComponentType {
+        NONE,
+        PLAY,
+        SETTINGS,
+        QUIT
+    };
+
+    struct InfoBox {
+        float x;
+        float y;
+        float width;
+        float height;
+    };
+
+    struct Text {
+        std::string contentText;
+        pos_t pos;
+        int fontSize;
+        Color color;
+    };
+
+    struct Box {
+        Rectangle box;
+        Color color;
+    };
+
+    struct Component {
+        std::vector<Box> boxes;
+        std::vector<Text> texts;
+        ComponentType cmpType;
+    };
+
     class Menu {
         public:
-            Menu(std::shared_ptr<GameState> gameState);
+            Menu(std::shared_ptr<GameState> gameState, int window_width, int window_height);
             ~Menu() = default;
 
-            bool isInMenu = false;
+            void renderMenu();
+            void handleEventMenu();
+            void handleEventEachComponent();
+            void resizeMenu(int window_width, int window_height);
 
+            void render(Component &component);
+
+            void componentPlay();
+            void componentSettings();
+            void componentQuit();
+
+            void handleEventEachComponent(Gui::Component component);
+
+            void addBox(const InfoBox &infoBox, Color color);
+            void addText(const Text &text);
+            void addComponent(ComponentType cmpType);
+
+            bool isInMenu = true;
         private:
             std::shared_ptr<GameState> _gameState;
 
+            std::vector<Gui::Component> _componentList;
+
+            int window_width = 0;
+            int window_height = 0;
+
+            Component _component;
     };
 }
 
