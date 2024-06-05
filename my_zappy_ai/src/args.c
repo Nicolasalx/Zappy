a8b34324822ed753d850110721f29e287f0bc19c
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-05 10:30:36 +0200

    add: args.c file with command line arguments

---
+++ my_zappy_ai/src/args.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** args
+*/
+
+#include "zappy_ai.h"
+
+const char *args[] = {
+    "-p",
+    "-h",
+    "-n"
+};
