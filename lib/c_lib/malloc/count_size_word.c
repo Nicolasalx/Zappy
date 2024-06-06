57820735a8042ffdd34d832822c639340e99d17f
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-06 12:31:57 +0200

    fix: coding style in server and ai

--- lib/c_lib/malloc/count_size_word.c
+++ lib/c_lib/malloc/count_size_word.c
@@ -8,6 +8,14 @@
 #include "my_string.h"
 #include "my_malloc.h"
 
+static void count_size_word_helper(int *size_word,
+    int count_size_word, bool end_word, int index)
+{
+    if (end_word) {
+        size_word[index] = count_size_word;
+    }
+}
+
 int *count_size_word(const char *str, const char *delimiter, int nb_word)
 {
     int *size_word = malloc_array(nb_word);
@@ -28,6 +36,6 @@ int *count_size_word(const char *str, const char *delimiter, int nb_word)
             count_size_word = 0;
         }
     }
-    end_word ? size_word[index] = count_size_word : true;
+    count_size_word_helper(size_word, count_size_word, end_word, index);
     return size_word;
 }
