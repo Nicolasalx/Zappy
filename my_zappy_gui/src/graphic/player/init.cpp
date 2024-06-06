0fde9b84fdf00cf3ad9413f24ac164eb4477365c
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:17:16 +0200

    add: initialization of the player in the gui part

--- my_zappy_gui/src/graphic/player/init.cpp
+++ my_zappy_gui/src/graphic/player/init.cpp
@@ -5,27 +5,14 @@
 ** initPlayer
 */
 
-#include "zappy_gui.hpp"
-#include "player.hpp"
+#include "Player.hpp"
 
-Gui::Player::Player(std::shared_ptr<GameState> gameState): _gameState(gameState)
+Gui::Player::Player()
 {
-    this->addModel("assets/Astronaut.iqm", "assets/AstronautColor.png");
 
-    int animsCount = 5;
-    this->player_animation = LoadModelAnimations("assets/Astronaut.iqm", &animsCount);
+}
 
-    this->player_orientation[1] = 180;
-    this->player_orientation[2] = 90;
-    this->player_orientation[3] = 0;
-    this->player_orientation[4] = 270;
+Gui::Player::~Player()
+{
 
-    this->player_color[1] = WHITE;
-    this->player_color[2] = RED;
-    this->player_color[3] = GREEN;
-    this->player_color[4] = BLUE;
-    this->player_color[5] = YELLOW;
-    this->player_color[6] = PURPLE;
-    this->player_color[7] = PINK;
-    this->player_color[8] = ORANGE;
 }
