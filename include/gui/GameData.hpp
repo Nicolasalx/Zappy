b91e6488dc0b71621457bdca67e3a64fd10a3e13
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:06:01 +0200

    add: new strucutre who handle all data between the client and the render

---
+++ include/gui/GameData.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** GameData
+*/
+
+#ifndef GAMEDATA_HPP_
+    #define GAMEDATA_HPP_
+
+namespace Gui
+{
+    struct DataCamera {
+        Camera3D _data;
+    };
+
+    struct GameData
+    {
+        DataCamera;
+        // handle Player
+        // handle SkyBox
+        // ...
+    };
+}
+
+#endif /* !GAMEDATA_HPP_ */
