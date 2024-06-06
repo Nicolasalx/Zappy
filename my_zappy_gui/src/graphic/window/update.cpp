cdbd2d6c3e212ad59d249b63f85238bb2623698c
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:20:24 +0200

    add: function to update window has been added in the gui part

---
+++ my_zappy_gui/src/graphic/window/update.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** updateObject
+*/
+
+#include "Object.hpp"
+
+const Gui::GameData &Gui::Object::update(const Gui::Event &event)
+{
+
+}
