21634a3cb7b2bf62a4961438dd2759b7bc0cf7e1
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:14:33 +0200

    add: new implementation of the render of the menu in the gui part

---
+++ my_zappy_gui/src/graphic/menu/update.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** updateMenu
+*/
+
+#include "Menu.hpp"
+
+const Gui::GameData &Gui::Menu::update(const Gui::Event &event)
+{
+
+}
