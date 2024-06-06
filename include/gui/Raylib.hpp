a560febe775a5326a42f1b812693da57dd07f412
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-06 17:47:00 +0200

    Add Raylib.hpp to include/gui directory

---
+++ include/gui/Raylib.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** Player
+*/
+
+#ifndef RAYLIB_H_
+    #define RAYLIB_H_
+
+    #include "ARenderModule.hpp"
+    #include <raylib.h>
+    #include <iostream>
+
+namespace Gui {
+    class Raylib : public ARenderModule
+    {
+        private:
+    };
+}
+
+#endif /* !RAYLIB_H_ */
