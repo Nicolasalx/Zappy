1184ce31dd0036b3d28d8c8bab8e5b917f29e0a4
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:20:04 +0200

    add: initialization of the window has been added in the gui part

---
+++ my_zappy_gui/src/graphic/window/init.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** initWindow
+*/
+
+#include "Window.hpp"
+
+Gui::Window::Window()
+{
+
+}
+
+Gui::Window::~Window()
+{
+
+}
