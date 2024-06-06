0429521560f7dea61929ac36be136d85cd0a95a0
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:06:50 +0200

    add: new class to render the gui part with a generic method

---
+++ include/gui/IRenderModule.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** IRendermodule
+*/
+
+#ifndef IRENDERMODULE_H_
+    #define IRENDERMODULE_H_
+
+    #include "GameData.hpp"
+    #include "Event.hpp"
+
+namespace Gui
+{
+    class IRenderModule {
+        public:
+            virtual ~IRenderModule() = default;
+            virtual Event getEvent() = 0;
+            virtual void render(const Gui::Gamedata &gameData) = 0;
+    };
+}
+
+#endif /* !IRENDERMODULE_H_ */
