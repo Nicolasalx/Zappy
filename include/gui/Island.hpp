524659857da97959c6142e4e698dc0769ec4b26c
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:07:07 +0200

    add: Class who handle all island has been added

---
+++ include/gui/Island.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** Island
+*/
+
+#ifndef ISLAND_HPP_
+    #define ISLAND_HPP_
+
+    #include "AGameModule.hpp"
+
+namespace Gui
+{
+    class Island: public Gui::AGameModule
+    {
+        public:
+            Island();
+            ~Island() override;
+    
+            const Gui::GameData &update(const Gui::Event &event) override;
+    
+        private:
+    
+    };
+}
+
+#endif /* !ISLAND_H_ */
