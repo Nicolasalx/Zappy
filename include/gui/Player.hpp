b7fcb64e62ea0d0cdf7880611b4f3b6a3719e377
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:08:30 +0200

    add: Class to handle all the player in the gui part has been added

---
+++ include/gui/Player.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** Player
+*/
+
+#ifndef PLAYER_HPP_
+    #define PLAYER_HPP_
+
+    #include "AGameModule.hpp"
+
+namespace Gui
+{
+    class Player: public Gui::AGameModule
+    {
+        public:
+            Player();
+            ~Player() override;
+    
+            const Gui::GameData &update(const Gui::Event &event) override;
+    
+        private:
+    
+    };
+}
+
+#endif /* !PLAYER_H_ */
