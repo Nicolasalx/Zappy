29962a450c34c76b990e69eea1a7edfdbd263f38
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:18:06 +0200

    add: initialization of the skybox has been added in the gui part

--- my_zappy_gui/src/graphic/skyBox/init.cpp
+++ my_zappy_gui/src/graphic/skyBox/init.cpp
@@ -5,22 +5,14 @@
 ** initSkyBox
 */
 
-#include "zappy_gui.hpp"
-#include "skyBox.hpp"
+#include "SkyBox.hpp"
 
-Gui::SkyBox::SkyBox(std::shared_ptr<GameState> gameState): _gameState(gameState)
+Gui::SkyBox::SkyBox()
 {
-    Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
-    this->model = LoadModelFromMesh(cube);
-    this->model.materials[0].shader = LoadShader(TextFormat("assets/skybox.vs", 330),
-                                            TextFormat("assets/skybox.fs", 330));
-    int environmentMap = MATERIAL_MAP_CUBEMAP;
-    int doGamma = 0;
-    int vflipped = 0;
-    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "environmentMap"), &environmentMap, SHADER_UNIFORM_INT);
-    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "doGamma"), &doGamma, SHADER_UNIFORM_INT);
-    SetShaderValue(model.materials[0].shader, GetShaderLocation(model.materials[0].shader, "vflipped"), &vflipped, SHADER_UNIFORM_INT);
-    Image img = LoadImage("assets/skybox3.png");
-    model.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
-    UnloadImage(img);
+
+}
+
+Gui::SkyBox::~SkyBox()
+{
+
 }
