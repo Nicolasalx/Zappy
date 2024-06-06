8ccfa4a7a5e5a92e1a2361a092b2d9d350cb9e99
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:17:46 +0200

    add: update function has been added in the player class in the gui part

---
+++ my_zappy_gui/src/graphic/player/update.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** updatePlayer
+*/
+
+#include "Player.hpp"
+
+const Gui::GameData &Gui::Player::update(const Gui::Event &event)
+{
+
+}
