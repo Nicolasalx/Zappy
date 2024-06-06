b62b8d0930fd7a8eb08913e3bece638ab0d23061
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:18:49 +0200

    add: initialization of the textBox has been added in the gui part

---
+++ my_zappy_gui/src/graphic/textBox/init.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** initTextBox
+*/
+
+#include "TextBox.hpp"
+
+Gui::TextBox::TextBox()
+{
+
+}
+
+Gui::TextBox::~TextBox()
+{
+
+}
