77350c1dc5e182844e6085cd2fceba7a3ef93cfc
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-06 11:18:30 +0200

    add: Skybox function to update data has been added in the gui part

---
+++ my_zappy_gui/src/graphic/skyBox/update.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** updateSkyBox
+*/
+
+#include "SkyBox.hpp"
+
+const Gui::GameData &Gui::SkyBox::update(const Gui::Event &event)
+{
+
+}
