7b02c3ffca6ba7821d7d11b7071a1c2527d9fef7
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-07 23:30:56 +0200

    refactor: Update RenderEgg class to use GameConfig instead of infoGame and raylibInfo

--- my_zappy_gui/render/egg/render.cpp
+++ my_zappy_gui/render/egg/render.cpp
@@ -7,16 +7,16 @@
 
 #include "RenderEgg.hpp"
 
-void Gui::RenderEgg::render(std::shared_ptr<Gui::GameData> gameData)
+void Gui::RenderEgg::render(const Gui::GameData &gameData)
 {
-    for (auto it = gameData->eggList.begin(); it != gameData->eggList.end(); ++it) {
+    for (auto it = gameData.eggList.begin(); it != gameData.eggList.end(); ++it) {
         this->drawModel(
-            ModelInfo(0,
+            (Gui::ModelInfo){0,
                 {
                     it->pos.x * Gui::MAP_SCALE - 2.3f,
                     1.0f,
                     it->pos.y * Gui::MAP_SCALE - 2.3f
                 },
-                {0, 0, 0}, 0, {1, 1, 1}, WHITE));
+                {0, 0, 0}, 0, {1, 1, 1}, WHITE});
     }
 }
