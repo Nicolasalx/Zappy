74c829ee1dcf870a92e7f0e146403f9564da4387
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-07 17:34:17 +0200

    add: render.so gui rendering

---
+++ my_zappy_gui/render/src/render.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** render
+*/
+
+#include "Raylib.hpp"
+
+void Gui::Raylib::render(const Gui::GameData &gameData)
+{
+    BeginMode3D(this->camera._data);
+    // BeginShaderMode(light_shader);
+    this->skyBox->render();
+    this->player->drawPlayer(this->rayInfo);
+    this->object->render();
+    this->particle.drawParticle(this->camera._data);
+    this->island->drawMap(this->rayInfo);
+    this->egg->render();
+    // EndShaderMode();
+    EndMode3D();
+}
