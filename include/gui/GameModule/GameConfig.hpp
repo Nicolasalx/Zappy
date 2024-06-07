c0f4f0904fd678d33285794b2805756b30d9821c
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-07 23:29:28 +0200

    feat: Add player, island, and item models and textures

--- include/gui/GameModule/GameConfig.hpp
+++ include/gui/GameModule/GameConfig.hpp
@@ -18,9 +18,27 @@ namespace Gui
     constexpr std::string_view SKYBOX_VERTEX_SHADER = "bonus/assets/skybox.vs";
     constexpr std::string_view SKYBOX_FRAGMENT_SHADER = "bonus/assets/skybox.fs";
     constexpr std::string_view SKYBOX_TEXTURE = "bonus/assets/skybox3.png";
-    
-    constexpr 
 
+    constexpr std::string_view PLAYER_MODEL = "bonus/assets/Astronaut.iqm";
+    constexpr std::string_view PLAYER_TEXTURE = "bonus/assets/AstronautColor.png";
+
+    constexpr std::string_view ISLAND_MODEL = "bonus/assets/plateform.obj";
+    constexpr std::string_view ISLAND_TEXTURE = "bonus/assets/plateform1.png";
+
+    constexpr std::string_view FOOD_MODEL = "bonus/assets/food.glb";
+    constexpr std::string_view FOOD_TEXTURE = "bonus/assets/texture_food.png";
+    constexpr std::string_view LINEMATE_MODEL = "bonus/assets/linemate.obj";
+    constexpr std::string_view LINEMATE_TEXTURE = "bonus/assets/texture_linemate.png";
+    constexpr std::string_view DERAUMERE_MODEL = "bonus/assets/deraumere.obj";
+    constexpr std::string_view DERAUMERE_TEXTURE = "bonus/assets/texture_deraumere.png";
+    constexpr std::string_view SIBUR_MODEL = "bonus/assets/sibur.obj";
+    constexpr std::string_view SIBUR_TEXTURE = "bonus/assets/texture_sibur.png";
+    constexpr std::string_view MENDIANE_MODEL = "bonus/assets/mendiane.obj";
+    constexpr std::string_view MENDIANE_TEXTURE = "bonus/assets/texture_mendiane.png";
+    constexpr std::string_view PHIRAS_MODEL = "bonus/assets/phiras.obj";
+    constexpr std::string_view PHIRAS_TEXTURE = "bonus/assets/texture_phiras.png";
+    constexpr std::string_view THYSTAME_MODEL = "bonus/assets/thystame.obj";
+    constexpr std::string_view THYSTAME_TEXTURE = "bonus/assets/texture_thystame.png";
 }
 
 #endif /* !GAMECONFIG_HPP_ */
