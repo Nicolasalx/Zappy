d55556ab895c2644a438dd390702c5a9a94209dd
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-07 12:03:59 +0200

    add: client.so lib for the gui client

---
+++ include/gui/client/Client.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** Client
+*/
+
+#ifndef CLIENT_HPP_
+    #define CLIENT_HPP_
+
+    #include "IClient.hpp"
+    #include <asio.hpp>
+
+namespace Gui
+{
+    class Client : public virtual Gui::IClient
+    {
+    private:
+        asio::io_context io_context_;
+        asio::ip::tcp::resolver resolver_;
+        asio::ip::tcp::resolver::results_type endpoint_;
+        asio::ip::tcp::socket socket_;
+
+        std::string buffer_;
+        static constexpr std::size_t buffer_size_ = 4096;
+
+        void bufferisingReply(std::string reply, std::vector<std::string> &message);
+
+    public:
+        Client(const std::string &ip, const std::string &port);
+        ~Client() override;
+
+        void send(const std::string &message) override;
+        std::vector<std::string> recv() override;
+    };
+}
+
+#endif /* !CLIENT_HPP_ */
