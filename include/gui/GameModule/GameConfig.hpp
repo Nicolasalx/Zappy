2ca1f67f40a5577a30ece1f93aacfcec971dcf9b
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-07 19:15:03 +0200

    add: config file for the zappy game

---
+++ include/gui/GameModule/GameConfig.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** GameConfig
+*/
+
+#ifndef GAMECONFIG_HPP_
+    #define GAMECONFIG_HPP_
+
+    #include <string>
+
+namespace Gui
+{
+    constexpr std::string_view EGG_MODEL = "bonus/assets/egg.obj";
+    constexpr std::string_view EGG_TEXTURE = "bonus/assets/texture_egg.png";
+}
+
+#endif /* !GAMECONFIG_HPP_ */
