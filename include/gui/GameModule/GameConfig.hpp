2654cde75fe72414298339e450f6151c70e5471f
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-07 20:52:48 +0200

    feat: Add skybox assets and shaders for the game module

--- include/gui/GameModule/GameConfig.hpp
+++ include/gui/GameModule/GameConfig.hpp
@@ -14,6 +14,13 @@ namespace Gui
 {
     constexpr std::string_view EGG_MODEL = "bonus/assets/egg.obj";
     constexpr std::string_view EGG_TEXTURE = "bonus/assets/texture_egg.png";
+
+    constexpr std::string_view SKYBOX_VERTEX_SHADER = "bonus/assets/skybox.vs";
+    constexpr std::string_view SKYBOX_FRAGMENT_SHADER = "bonus/assets/skybox.fs";
+    constexpr std::string_view SKYBOX_TEXTURE = "bonus/assets/skybox3.png";
+    
+    constexpr 
+
 }
 
 #endif /* !GAMECONFIG_HPP_ */
