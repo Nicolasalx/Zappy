66a82835c2f01ee98682155f8343ff4d985e12c0
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-07 23:31:14 +0200

    refactor: Update RenderPlayer to use GameData for rendering

--- my_zappy_gui/render/src/player.cpp
+++ my_zappy_gui/render/src/player.cpp
@@ -6,14 +6,15 @@
 */
 
 #include "Raylib.hpp"
-#include "player.hpp"
+#include "RenderPlayer.hpp"
+#include "GameConfig.hpp"
 
 Gui::RenderPlayer::RenderPlayer()
 {
-    this->addModel("bonus/assets/Astronaut.iqm", "bonus/assets/AstronautColor.png");
+    this->addModel(PLAYER_MODEL.data(), PLAYER_TEXTURE.data());
 
     int animsCount = 5;
-    this->player_animation = LoadModelAnimations("bonus/assets/Astronaut.iqm", &animsCount);
+    this->player_animation = LoadModelAnimations(PLAYER_MODEL.data(), &animsCount);
 
     this->player_orientation[1] = 180;
     this->player_orientation[2] = 90;
@@ -28,4 +29,22 @@ Gui::RenderPlayer::RenderPlayer()
     this->player_color[6] = PURPLE;
     this->player_color[7] = PINK;
     this->player_color[8] = ORANGE;
+}
+
+void Gui::RenderPlayer::drawPlayerModel(const Gui::Player &player)
+{
+    this->drawModel((ModelInfo){0, (Vector3){player.real_pos.x * Gui::MAP_SCALE, 0.0f, player.real_pos.y * Gui::MAP_SCALE}, (Vector3){0, 1, 0}, 
+    (float)player.real_orientation, (Vector3){3 + player.level / 10.0f, 3 + player.level / 10.0f, 3 + player.level / 10.0f}, player_color[player.level]});
+}
+
+void Gui::RenderPlayer::render(const Gui::GameData &gameData)
+{
+    for (auto &player : gameData.playerList) {
+        //updateAnimation(player);
+        // if (rayInfo.type == PLAYER && rayInfo.id == player.n) {
+        //     drawPlayerBox(rayInfo, player);
+        // } else {
+            drawPlayerModel(player);
+        //}
+    }
 }
