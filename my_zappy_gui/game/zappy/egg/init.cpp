d191cc1f826de237e9c2eb811169dd2ddadb4e6b
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-08 13:33:35 +0200

    feat: Add Egg class to Gui module

---
+++ my_zappy_gui/game/zappy/egg/init.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** initIsland
+*/
+
+#include "Egg.hpp"
+
+Gui::Egg::Egg(std::shared_ptr<Gui::GameData> gameData): _gameData(gameData)
+{
+
+}
