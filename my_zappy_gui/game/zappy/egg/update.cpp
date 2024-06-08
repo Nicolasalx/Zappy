ebf55c00dc1b90433423c7a6abcf733a66d63a9e
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-08 13:33:42 +0200

    feat: Implement Egg update function in Gui module

---
+++ my_zappy_gui/game/zappy/egg/update.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** updateIsland
+*/
+
+#include "Zappy.hpp"
+#include "Egg.hpp"
+
+void Gui::Egg::update(const Gui::Event &event)
+{
+
+}
