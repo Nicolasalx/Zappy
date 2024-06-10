363fb29ffc6a691eea6bcea3ad6aba70010e6a7e
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-10 18:59:04 +0200

    mod: menu has been updated in the gui part

--- my_zappy_gui/game/zappy/menu/init.cpp
+++ my_zappy_gui/game/zappy/menu/init.cpp
@@ -7,33 +7,15 @@
 
 #include "Menu.hpp"
 
-void Gui::Menu::addBox(const Box &box)
-{
-    this->_component.box = { box.x, box.y, box.width, box.height, box.color };
-    this->_component.stockStartData.box = box;
-}
-
-void Gui::Menu::addText(const Text &text)
-{
-    this->_component.text = text;
-}
-
-void Gui::Menu::addComponent(ComponentType cmpType)
-{
-    this->_component.componentType = cmpType;
-    this->_gameData->dataMenu.componentList.push_back(_component);
-    this->_component.componentType = NONE;
-}
-
 void Gui::Menu::componentPlay()
 {
-    addBox({Box(0.4, 0.2, 0.3, 0.2, GRAY_COLOR)});
-    addText(Text("PLAY", { 0.4, 0.4 }, 20, GRAY_COLOR));
+    addBox({Box(0.4, 0.2, 0.3, 0.2, BLUE_COLOR)});
+    addText(Text("PLAY", { 0.4, 0.4 }, 20, BLUE_COLOR));
 
     addComponent(PLAY);
 }
 
-Gui::Menu::Menu(std::shared_ptr<Gui::GameData> gameData) : _gameData(gameData)
+Gui::Menu::Menu(std::shared_ptr<Gui::GameData> gameData): _gameData(gameData)
 {
     componentPlay();
 }
