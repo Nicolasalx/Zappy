cc0483b14a60427ba22f386d90a7a3464c3644d3
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-10 16:59:33 +0200

    refactor: Update getEvent.cpp to fix window size assignment bug

--- my_zappy_gui/raylib_render/src/getEvent.cpp
+++ my_zappy_gui/raylib_render/src/getEvent.cpp
@@ -17,8 +17,8 @@ Gui::Event Gui::Raylib::getEvent()
     }
     if (IsWindowResized()) {
         event.eventType.push_back(Gui::EventType::WINDOW_RESIZED);
-        event.window_size_x = GetScreenWidth();
-        event.window_size_y = GetScreenHeight();
+        window.window_height = GetScreenHeight();
+        window.window_width = GetScreenWidth();
     }
     if (IsKeyPressed(KEY_N)) {
         event.eventType.push_back(Gui::EventType::NEXT_DISPLAY);
@@ -34,6 +34,8 @@ Gui::Event Gui::Raylib::getEvent()
         event.mouse_x = GetMouseX();
         event.mouse_y = GetMouseY();
     }
+    event.window_size_x = window.window_height;
+    event.window_size_y = window.window_width;
     this->camera.handle_cursor();
     this->camera.update();
     return event;
