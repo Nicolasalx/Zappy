2a4d09ec2e63c0fa49de37c15bb07a6b2eecd930
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-10 22:08:44 +0200

    add: egg rendering in SFMLRenderEgg

--- my_zappy_gui/sfml_render/src/egg.cpp
+++ my_zappy_gui/sfml_render/src/egg.cpp
@@ -9,15 +9,12 @@
 
 Gui::SFMLRenderEgg::SFMLRenderEgg(const GameData& gameData)
 {
-    textures.resize(gameData.playerList.size());
-    for (size_t i = 0; i < gameData.playerList.size(); ++i) {
-        if (!textures[i].loadFromFile("bonus/assets/astronaute.png")) {
-            std::cerr << "Error loading player texture" << std::endl;
+    egg_textures.resize(gameData.eggList.size());
+    for (size_t i = 0; i < gameData.eggList.size(); ++i) {
+        if (!egg_textures[i].loadFromFile("bonus/assets/egg.png")) {
+            std::cerr << "Error loading egg texture" << std::endl;
             return;
         }
-    }
-    if (!textures.empty()) {
-        sprite.setTexture(textures[0]);
     }
 }
 
@@ -26,14 +23,14 @@ void Gui::SFMLRenderEgg::render(const GameData& gameData)
     double cell_size = std::min(window.getSize().x / gameData.mapSize.x, window.getSize().y / gameData.mapSize.y);
     double offset_x = (window.getSize().x - (cell_size * gameData.mapSize.x)) / 2;
     double offset_y = (window.getSize().y - (cell_size * gameData.mapSize.y)) / 2;
-    double player_scale = cell_size / sprite.getLocalBounds().height * 0.75f;
 
-    for (size_t i = 0; i < gameData.playerList.size(); ++i) {
-        sprite.setTexture(textures[i]);
-        sprite.setScale(player_scale, player_scale);
-        double sprite_offset_x = 0.5 * cell_size - (sprite.getLocalBounds().width * sprite.getScale().x) / 2;
-        double sprite_offset_y = 0.5 * cell_size - (sprite.getLocalBounds().height * sprite.getScale().y) / 2;
-        sprite.setPosition(offset_x + gameData.playerList[i].pos.x * cell_size + sprite_offset_x, offset_y + gameData.playerList[i].pos.y * cell_size + sprite_offset_y);
+    for (int i = 0; i < gameData.eggList.size(); ++i) {
+        sf::Sprite sprite(egg_textures[i]);
+        float egg_scale = cell_size / sprite.getLocalBounds().height * scale;
+        sprite.setScale(egg_scale, egg_scale);
+        double egg_offset_x = 0.75 * cell_size - (egg_scale * sprite.getLocalBounds().width) / 2;
+        double egg_offset_y = 0.5 * cell_size - (egg_scale * sprite.getLocalBounds().height) / 2;
+        sprite.setPosition(offset_x + gameData.eggList[i].pos.x * cell_size + egg_offset_x, offset_y + gameData.eggList[i].pos.y * cell_size + egg_offset_y);
         window.draw(sprite);
     }
 }
