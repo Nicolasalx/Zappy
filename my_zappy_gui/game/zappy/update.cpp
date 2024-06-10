4c3d360425010fe258d6b3bd98aa2a55cb588e95
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-10 15:53:04 +0200

    refactor: Update Zappy class to include TextBox update in the update method

--- my_zappy_gui/game/zappy/update.cpp
+++ my_zappy_gui/game/zappy/update.cpp
@@ -9,16 +9,14 @@
 
 void Gui::Zappy::update(std::vector<std::string> &messRecv, const Gui::Event &event)
 {
-<<<<<<< HEAD
-=======
     this->_gameData->frameTime = event.frame_time;
 
->>>>>>> d5e87cb1d48e7497f8228a3a191d6722366478ac
     this->_parser.update(messRecv);
 
     this->_island.update(event);
     this->_object.update(event);
     this->_player.update(event);
     this->_egg.update(event);
+    this->_textBox.update(event);
     this->_menu.update(event);
 }
