99b983992120c2d4981442f68c02b2081abdb59d
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-11 16:51:56 +0200

    refactor: Add openClose function to TextBox class in GameModule

--- my_zappy_gui/game/zappy/textBox/update.cpp
+++ my_zappy_gui/game/zappy/textBox/update.cpp
@@ -16,18 +16,46 @@ void Gui::TextBox::addText(TextBoxData &textBox, size_t index, std::string text)
     }
 }
 
+void Gui::TextBox::openClose(const Gui::Event &event)
+{
+    for (auto &_button : _gameData->textBox) {
+        if (event.mouse.x > _button._buttonPos.x && event.mouse.x < _button._buttonPos.x + _button._buttonSize.x &&
+            event.mouse.y > _button._buttonPos.y && event.mouse.y < _button._buttonPos.y + _button._buttonSize.y) {
+            if (_button._state == RIGHT_BUTTON) {
+                _button._state = CLOSED_RIGHT;
+                resize(_gameData->windowX, _gameData->windowY);
+            }
+            else if (_button._state == LEFT_BUTTON) {
+                _button._state = CLOSED_LEFT;
+                resize(_gameData->windowX, _gameData->windowY);
+            }
+            else if (_button._state == CLOSED_RIGHT) {
+                _button._state = RIGHT_BUTTON;
+                resize(_gameData->windowX, _gameData->windowY);
+            }
+            else if (_button._state == CLOSED_LEFT) {
+                _button._state = LEFT_BUTTON;
+                resize(_gameData->windowX, _gameData->windowY);
+            }
+        }
+    }
+}
+
 void Gui::TextBox::updateGeneralInfo()
 {
     addText(_gameData->textBox[0], 0, "Number of players: " + std::to_string(_gameData->playerList.size()));
     addText(_gameData->textBox[1], 1, "Server frequency: " + std::to_string(_gameData->timeUnit));
 }
 
-void Gui::TextBox::update(const Gui::Event &event)
+void Gui::TextBox::update(const Gui::Event &events)
 {
     updateGeneralInfo();
-    for (auto &event : event.eventType) {
+    for (auto &event : events.eventType) {
         if (event == Gui::EventType::WINDOW_RESIZED) {
             resize(_gameData->windowX, _gameData->windowY);
+        }
+        if (event == Gui::EventType::LEFT_CLICK) {
+            openClose(events);
         }
     }
 }
