6535049aeee4d72932b07cd4342d936583d52646
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-11 17:10:37 +0200

    add: debug logging for right_cmd

--- my_zappy_server/src/ai_command/right_cmd.c
+++ my_zappy_server/src/ai_command/right_cmd.c
@@ -25,4 +25,7 @@ void right_cmd(char *, client_t *client, server_t *server)
     }
     send_msg_client(client->fd, "ok\n");
     ppo_reply(server, client);
+    if (server->opt.is_debug == true)
+        printf("[Debug] Player %d turned right, new position: X:%d Y:%d O:%d\n",
+            client->player.id, client->player.pos_x, client->player.pos_y, client->player.orientation);
 }
