b70e59e2e482eb32ef4fa195ceb178767880ff65
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-11 11:25:49 +0200

    add: SFMLRenderObject to render objects on the map

--- my_zappy_gui/sfml_render/src/sfml_render.cpp
+++ my_zappy_gui/sfml_render/src/sfml_render.cpp
@@ -20,6 +20,7 @@ Gui::SFMLRender::SFMLRender() : window(sf::VideoMode(1920, 1080), "ZAPPY")
     this->player = std::make_unique<Gui::SFMLRenderPlayer>();
     this->egg = std::make_unique<Gui::SFMLRenderEgg>();
     this->map = std::make_unique<Gui::SFMLRenderMap>();
+    this->object = std::make_unique<Gui::SFMLRenderObject>();
 }
 
 Gui::SFMLRender::~SFMLRender()
@@ -57,41 +58,10 @@ Gui::Event Gui::SFMLRender::getEvent()
 
 void Gui::SFMLRender::render(const Gui::GameData &gameData)
 {
-    std::vector<sf::Texture> object_textures(7);
-    for (int i = 0; i < 7; ++i) {
-        std::string filename = "bonus/assets/res_" + std::to_string(i + 1) + ".png";
-        if (!object_textures[i].loadFromFile(filename)) {
-            std::cerr << "Error loading object texture: " << filename << std::endl;
-            return;
-        }
-    }
     if (window.isOpen()) {
         window.clear();
-        double cell_size = std::min(window.getSize().x / gameData.mapSize.x, window.getSize().y / gameData.mapSize.y);
-        double offset_x = (window.getSize().x - (cell_size * gameData.mapSize.x)) / 2;
-        double offset_y = (window.getSize().y - (cell_size * gameData.mapSize.y)) / 2;
-        double scale = 0.15f;
-        std::pair<double, double> object_positions[7] = {{0.25, 0.25}, {0.50, 0.25}, {0.75, 0.25}, {0.25, 0.75}, {0.50, 0.75}, {0.75, 0.75}, {0.25, 0.50}};
-        for (int i = 0; i < gameData.mapSize.x; ++i) {
-            for (int j = 0; j < gameData.mapSize.y; ++j) {
-                // sf::Sprite sprite(map_texture[i * gameData.mapSize.y + j]);
-                // sprite.setScale(cell_size / sprite.getLocalBounds().height, cell_size / sprite.getLocalBounds().height);
-                // sprite.setPosition(offset_x + i * cell_size, offset_y + j * cell_size);
-                // window.draw(sprite);
-                for (int k = 0; k < 7; ++k) {
-                    if (gameData.objectPos[j][i][k] > 0) {
-                        sf::Sprite object_sprite(object_textures[k]);
-                        float object_scale = cell_size / object_sprite.getLocalBounds().height * scale;
-                        object_sprite.setScale(object_scale, object_scale);
-                        double object_offset_x = object_positions[k].first * cell_size - (object_scale * object_sprite.getLocalBounds().width) / 2;
-                        double object_offset_y = object_positions[k].second * cell_size - (object_scale * object_sprite.getLocalBounds().height) / 2;
-                        object_sprite.setPosition(offset_x + i * cell_size + object_offset_x, offset_y + j * cell_size + object_offset_y);
-                        window.draw(object_sprite);
-                    }
-                }
-            }
-        }
         this->map->render(gameData);
+        this->object->render(gameData);
         this->egg->render(gameData);
         this->player->render(gameData);
         window.display();
