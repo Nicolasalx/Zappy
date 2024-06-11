4b6fa3e3260bfd427bb948c11130bb3a16bc78ed
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-11 16:51:38 +0200

    refactor: Adjust TextBox initialization in init.cpp for Zappy GUI

--- my_zappy_gui/game/zappy/textBox/init.cpp
+++ my_zappy_gui/game/zappy/textBox/init.cpp
@@ -22,11 +22,11 @@ Gui::TextBoxData::TextBoxData(float x, float y, float width, float height, float
 
 Gui::TextBox::TextBox(std::shared_ptr<Gui::GameData> gameData) : _gameData(gameData)
 {
-    TextBoxData boxInfo(0.77f, 0.47f, 0.21f, 0.51f, 0.016, LEFT_BUTTON);
+    TextBoxData boxInfo(0.77f, 0.04f, 0.21f, 0.1f, 0.016, LEFT_BUTTON);
     _gameData->textBox.push_back(boxInfo);
-    TextBoxData boxInfo1(0.77f, 0.47f, 0.21f, 0.40f, 0.016, 0);
+    TextBoxData boxInfo1(0.77f, 0.47f, 0.21f, 0.51f, 0.016, 0);
     _gameData->textBox.push_back(boxInfo1);
-    TextBoxData boxInfo2(0.77f, 0.57f, 0.21f, 0.40f, 0.016, 0);
+    TextBoxData boxInfo2(0.77f, 0.47f, 0.21f, 0.40f, 0.016, 0);
     _gameData->textBox.push_back(boxInfo2);
     TextBoxData boxInfo3(0.01f, 0.57f, 0.15f, 0.40f, 0.016, RIGHT_BUTTON);
     _gameData->textBox.push_back(boxInfo3);
