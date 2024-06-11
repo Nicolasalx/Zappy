4128a857b3ae16b083451d097d3c892f867c9f5f
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-11 11:14:44 +0200

    refactor: SFMLRenderEgg class and add LoadEggTexture method

--- my_zappy_gui/sfml_render/src/egg.cpp
+++ my_zappy_gui/sfml_render/src/egg.cpp
@@ -7,26 +7,31 @@
 
 #include "SFMLRenderEgg.hpp"
 
-Gui::SFMLRenderEgg::SFMLRenderEgg(const GameData& gameData)
+Gui::SFMLRenderEgg::SFMLRenderEgg()
 {
-    egg_textures.resize(gameData.eggList.size());
+    ;
+}
+
+void Gui::SFMLRenderEgg::LoadEggTexture(const GameData &gameData)
+{
+    textures.resize(gameData.eggList.size());
     for (size_t i = 0; i < gameData.eggList.size(); ++i) {
-        if (!egg_textures[i].loadFromFile("bonus/assets/egg.png")) {
+        if (!textures[i].loadFromFile("bonus/assets/egg.png")) {
             std::cerr << "Error loading egg texture" << std::endl;
             return;
         }
     }
 }
 
-void Gui::SFMLRenderEgg::render(const GameData& gameData)
+void Gui::SFMLRenderEgg::render(const GameData &gameData)
 {
     double cell_size = std::min(window.getSize().x / gameData.mapSize.x, window.getSize().y / gameData.mapSize.y);
     double offset_x = (window.getSize().x - (cell_size * gameData.mapSize.x)) / 2;
     double offset_y = (window.getSize().y - (cell_size * gameData.mapSize.y)) / 2;
 
-    for (int i = 0; i < gameData.eggList.size(); ++i) {
-        sf::Sprite sprite(egg_textures[i]);
-        float egg_scale = cell_size / sprite.getLocalBounds().height * scale;
+    for (size_t i = 0; i < gameData.eggList.size(); ++i) {
+        sf::Sprite sprite(textures[i]);
+        float egg_scale = cell_size / sprite.getLocalBounds().height * 0.15f;
         sprite.setScale(egg_scale, egg_scale);
         double egg_offset_x = 0.75 * cell_size - (egg_scale * sprite.getLocalBounds().width) / 2;
         double egg_offset_y = 0.5 * cell_size - (egg_scale * sprite.getLocalBounds().height) / 2;
