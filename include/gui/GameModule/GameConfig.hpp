78e7f82d8baf4f86b3b6149b6e5d821906725731
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-11 22:53:48 +0200

    add: new menu model

--- include/gui/GameModule/GameConfig.hpp
+++ include/gui/GameModule/GameConfig.hpp
@@ -42,7 +42,7 @@ namespace Gui
 
     constexpr std::string_view EVOLUTION_TEXTURE = "bonus/assets/evolution.png";
 
-    constexpr std::string_view MENU_MARS_MODEL = "new_menu_scene.obj";
+    constexpr std::string_view MENU_MARS_MODEL = "menu_scene.glb";
 }
 
 #endif /* !GAMECONFIG_HPP_ */
