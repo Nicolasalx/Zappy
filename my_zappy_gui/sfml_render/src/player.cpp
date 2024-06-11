dd5efe2c523e9e799cdd090bc8199042ae301d22
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-11 22:17:16 +0200

    add: player sprite and position

--- my_zappy_gui/sfml_render/src/player.cpp
+++ my_zappy_gui/sfml_render/src/player.cpp
@@ -20,11 +20,12 @@ void Gui::SFMLRenderPlayer::render(const GameData &gameData, sf::RenderWindow &w
     double player_scale = cell_size / sprite.getLocalBounds().height * 0.75f;
 
     for (size_t i = 0; i < gameData.playerList.size(); ++i) {
-        sprite.setTexture(this->getTexture("bonus/assets/player.png"));
+        sprite.setTexture(this->getTexture("bonus/assets/astronaute.png"));
         sprite.setScale(player_scale, player_scale);
         double sprite_offset_x = 0.5 * cell_size - (sprite.getLocalBounds().width * sprite.getScale().x) / 2;
         double sprite_offset_y = 0.5 * cell_size - (sprite.getLocalBounds().height * sprite.getScale().y) / 2;
-        sprite.setPosition(offset_x + gameData.playerList[i].pos.x * cell_size + sprite_offset_x, offset_y + gameData.playerList[i].pos.y * cell_size + sprite_offset_y);
+        sprite.setPosition(offset_x + gameData.playerList[i].real_pos.x * cell_size + sprite_offset_x, offset_y + gameData.playerList[i].real_pos.y * cell_size + sprite_offset_y);
+        std::cout << "pos x: " << gameData.playerList[i].pos.x << " , real pos_x: " << gameData.playerList[i].real_pos.x << std::endl;
         window.draw(sprite);
     }
 }
