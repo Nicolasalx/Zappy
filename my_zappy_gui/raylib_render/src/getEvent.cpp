789d2123d1656c836767bc81f3e9aad248449ac2
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-12 19:31:55 +0200

    mod: system to get the event has been changed in the raylib

--- my_zappy_gui/raylib_render/src/getEvent.cpp
+++ my_zappy_gui/raylib_render/src/getEvent.cpp
@@ -7,15 +7,50 @@
 
 #include "Raylib.hpp"
 
+// ! https://www.raylib.com/examples/text/loader.html?name=text_input_box
+
+void Gui::Raylib::putEventInBuffer(Gui::Event &event)
+{
+    int charPressed = GetCharPressed();
+
+    if (charPressed == 0) {
+        return;
+    }
+    if (IsKeyPressed(KEY_ENTER)) {
+        event.eventType.push_back(Gui::EventType::ENTER);
+    } else if (charPressed == 61) { // ! Actually press '=' to delete one character
+        event.buffer.pop_back();
+        std::cout << "BUFF: " << event.buffer << "\n";
+        _event.buffer = event.buffer;
+    } else {
+        _event.buffer += (char) charPressed;
+    }
+}
+
+void Gui::Raylib::putEventInEventList(Gui::Event &event)
+{
+    for (const auto &currentKey : this->_keyBind) {
+        if (IsKeyPressed(currentKey.first)) {
+            event.eventType.push_back(currentKey.second);
+        }
+    }
+}
+
+void Gui::Raylib::getKeyEvent(Gui::Event &event)
+{
+    if (this->_ignoreKey) {
+        this->putEventInBuffer(event);
+    } else {
+        this->putEventInEventList(event);
+    }
+    event.buffer = this->_event.buffer;
+}
+
 Gui::Event Gui::Raylib::getEvent()
 {
     Gui::Event event;
 
     event.frame_time = GetFrameTime();
-    if (IsKeyPressed(KEY_M)) {
-        std::cout << "KEY M EVENT\n";
-        event.eventType.push_back(Gui::EventType::KEY_M);
-    }
     if (WindowShouldClose()) {
         event.eventType.push_back(Gui::EventType::EXIT);
     }
@@ -24,6 +59,16 @@ Gui::Event Gui::Raylib::getEvent()
         window.windowSize.width = GetScreenWidth();
         window.windowSize.height = GetScreenHeight();
         window.launched = true;
+    }
+
+    if (this->menu.stateGame == IN_LEAVE) {
+        event.eventType.push_back(Gui::EventType::EXIT);
+    }
+    getKeyEvent(event);
+
+    if (IsKeyPressed(KEY_M)) {
+        std::cout << "KEY M EVENT\n";
+        event.eventType.push_back(Gui::EventType::KEY_M);
     }
     if (IsKeyPressed(KEY_N)) {
         event.eventType.push_back(Gui::EventType::NEXT_DISPLAY);
@@ -34,6 +79,7 @@ Gui::Event Gui::Raylib::getEvent()
     if (IsKeyPressed(KEY_O)) {
         event.eventType.push_back(Gui::EventType::KEY_O);
     }
+
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         event.eventType.push_back(Gui::EventType::LEFT_CLICK);
     }
