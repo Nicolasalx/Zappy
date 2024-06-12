3a36b6da4924dedeb2603b0734dcfc0c356ff598
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-12 12:32:59 +0200

    Add RenderRayInfo.hpp file

---
+++ include/gui/RenderModule/RayLibRender/RenderRayInfo.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** player
+*/
+
+#ifndef RENDERRAYINFO_H_
+    #define RENDERRAYINFO_H_
+
+#endif /* !RENDERRAYINFO_H_ */
