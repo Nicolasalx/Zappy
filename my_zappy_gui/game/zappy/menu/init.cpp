91624e41cc275d836cdf6fac2788edc5749d21b6
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-12 11:47:33 +0200

    del: remove from the initialization of the menu the component PLAY

--- my_zappy_gui/game/zappy/menu/init.cpp
+++ my_zappy_gui/game/zappy/menu/init.cpp
@@ -8,41 +8,34 @@
 #include "Menu.hpp"
 #include "GameConfig.hpp"
 
-void Gui::Menu::componentPlay()
-{
-    addBox({Box(0.1, 0.5, 0.4, 0.1, BLUE_COLOR)}, "PLAY");
-    addComponent(PLAY);
-}
-
 void Gui::Menu::componentSettings()
 {
-    addBox({Box(0.1, 0.7, 0.4, 0.1, BLUE_COLOR)}, "SETTINGS");
+    addBox({Box(0.1, 0.6, 0.4, 0.1, BLUE_COLOR)}, "SETTINGS");
     addComponent(SETTINGS);
 }
 
 void Gui::Menu::componentQuit()
 {
-    addBox({Box(0.1, 0.9, 0.4, 0.1, BLUE_COLOR)}, "QUIT");
+    addBox({Box(0.1, 0.8, 0.4, 0.1, BLUE_COLOR)}, "QUIT");
     addComponent(QUIT);
 }
 
 void Gui::Menu::componentPlayerMode()
 {
-    addBox({Box(0.1, 0.1, 0.4, 0.1, BLUE_COLOR)}, "PLAYER MODE");
+    addBox({Box(0.1, 0.2, 0.4, 0.1, BLUE_COLOR)}, "PLAYER MODE");
     addComponent(PLAYER_MODE);
 }
 
 void Gui::Menu::componentSpectatorMode()
 {
-    addBox({Box(0.1, 0.3, 0.4, 0.1, BLUE_COLOR)}, "SPECTATOR MODE");
+    addBox({Box(0.1, 0.4, 0.4, 0.1, BLUE_COLOR)}, "SPECTATOR MODE");
     addComponent(SPECTATOR_MODE);
 }
 
 Gui::Menu::Menu(std::shared_ptr<Gui::GameData> gameData): _gameData(gameData)
 {
-    componentPlay();
-    componentSettings();
     componentPlayerMode();
     componentSpectatorMode();
+    componentSettings();
     componentQuit();
 }
