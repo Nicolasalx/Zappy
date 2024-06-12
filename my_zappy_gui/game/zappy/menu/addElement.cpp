522730ab839eaa712d08a22bc3983acf66dd4ebd
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-12 11:39:58 +0200

    mod: system to add a new element in the menu has changed, now the text and the box are linked together to not handle the position of the text

--- my_zappy_gui/game/zappy/menu/addElement.cpp
+++ my_zappy_gui/game/zappy/menu/addElement.cpp
@@ -6,17 +6,22 @@
 */
 
 #include "Menu.hpp"
+#include "GameConfig.hpp"
 
-void Gui::Menu::addBox(const Box &box)
+void Gui::Menu::addBox(const Box &box, const std::string &contentText)
 {
     this->_component.stockStartData.box = box;
     this->_component.box = { box.x, box.y, box.width, box.height, box.color };
     this->_component.stockStartData.box = this->_component.box;
+
+    double startTextPosX = (box.width - (contentText.size() * 0.02)) / 2;
+    addText(Text(contentText, { box.x + startTextPosX , box.y + 0.03 }, sizeFontTextMenu, BLUE_COLOR));
 }
 
 void Gui::Menu::addText(const Text &text)
 {
     this->_component.text = text;
+    this->_component.stockStartData.text = text;
 }
 
 void Gui::Menu::addComponent(ComponentType cmpType)
