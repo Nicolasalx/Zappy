087e87aa8a9b6716345e807d603b5ebaa5029e0e
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-06-12 15:21:35 +0200

    refactor: Update RenderRayInfo to include RayInfoData struct and remove unused variables

--- include/gui/RenderModule/RayLibRender/RenderRayInfo.hpp
+++ include/gui/RenderModule/RayLibRender/RenderRayInfo.hpp
@@ -7,5 +7,31 @@
 
 #ifndef RENDERRAYINFO_H_
     #define RENDERRAYINFO_H_
+    #include <raylib.h>
+
+namespace Gui {
+
+    struct RayInfoData {
+        int type = 0;
+        int id = 0;
+        int x = 0;
+        int y = 0;
+    };
+
+    class RayInfo {
+        public:
+            RayInfo() = default;
+            ~RayInfo() = default;
+            
+        private:
+            Ray ray;
+            BoundingBox box;
+            RayCollision collision;
+            int type = 0;
+            int id = 0;
+            int x = 0;
+            int y = 0;
+    };
+}
 
 #endif /* !RENDERRAYINFO_H_ */
