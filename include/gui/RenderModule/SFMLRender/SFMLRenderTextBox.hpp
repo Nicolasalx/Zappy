60515dc2dad553e1529b4b4d14a4c95f8042cb84
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-13 19:41:14 +0200

    add: SFMLRenderTextBox class and add SFMLRenderTextBoxList class

--- include/gui/RenderModule/SFMLRender/SFMLRenderTextBox.hpp
+++ include/gui/RenderModule/SFMLRender/SFMLRenderTextBox.hpp
@@ -2,24 +2,47 @@
 ** EPITECH PROJECT, 2024
 ** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
 ** File description:
-** RenderTextBox
+** textBox
 */
 
-#include <memory>
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include "GameData.hpp"
-#include "SFMLPool.hpp"
+#ifndef RENDERTEXTBOX_H_
+    #define RENDERTEXTBOX_H_
+
+    #include "GameData.hpp"
+    #include "SFML/Graphics.hpp"
+    #include <complex>
+    #include <vector>
+    #include <memory>
 
 namespace Gui
 {
-    class SFMLRenderTextBox : Gui::SFMLPool
-    {
+    class SFMLRenderTextBox {
         public:
-            SFMLRenderTextBox();
-            void render(const Gui::GameData &gameData, sf::RenderWindow &window);
-        private:
-            sf::Sprite sprite;
+            SFMLRenderTextBox(TextBoxData boxInfo);
+            ~SFMLRenderTextBox() = default;
+            void openClose();
+            bool isClosed();
+            void resize(int window_width, int window_height);
+            void addText(sf::RenderWindow &window, const std::string &text, float ypos, const sf::Font &font);
+            void draw(sf::RenderWindow &window, const sf::Font &font);
+            TextBoxData _boxInfo;
+            // Rectangle _box;
+            // Rectangle _button;
+            // Color _color;
+            sf::FloatRect _box;
+            sf::FloatRect _button;
+            sf::Color _color;
+    };
+
+    class SFMLRenderTextBoxList {
+        public:
+            SFMLRenderTextBoxList() = default;
+            ~SFMLRenderTextBoxList() = default;
+            void updateTextBoxs(const GameData &gameData);
+            void drawAllTextBoxs(sf::RenderWindow &window, const sf::Font &font, const GameData &gameData);
+
+            std::vector<Gui::SFMLRenderTextBox> _textBoxs;
     };
 }
+
+#endif
