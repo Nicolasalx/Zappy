8f7746c9f71382e44bf60d6abd9cc7b4461d3e50
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-13 19:41:21 +0200

    add: font loading for text box rendering

--- my_zappy_gui/sfml_render/src/sfml_render.cpp
+++ my_zappy_gui/sfml_render/src/sfml_render.cpp
@@ -64,6 +64,8 @@ void Gui::SFMLRender::render(const Gui::GameData &gameData)
 {
     sf::Texture newTexture;
     sf::Sprite sprite;
+    sf::Font font;
+    font.loadFromFile("bonus/assets/font.ttf");
 
     newTexture.loadFromFile("bonus/assets/background.png");
     sprite.setTexture(newTexture);
@@ -78,7 +80,7 @@ void Gui::SFMLRender::render(const Gui::GameData &gameData)
     this->object->render(gameData, window);
     this->egg->render(gameData, window);
     this->player->render(gameData, window);
-    this->textBox->render(gameData, window);
+    this->textBoxList->drawAllTextBoxs(window, font, gameData);
     // if (std::find(eventList.eventType.begin(), eventList.eventType.end(), Gui::EventType::LEFT_CLICK) != eventList.eventType.end()) {
     //     sf::Vector2i cell = getClickedCase(gameData, eventList.mouse.x, eventList.mouse.y);
     //     if (cell.x != -1 && cell.y != -1) {
