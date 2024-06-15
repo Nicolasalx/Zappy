0a6a25900168caf27c6cec61d7725d3c947f1537
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-15 13:05:29 +0200

    add: player redering parser

---
+++ my_zappy_gui/game/zappy/player_parser/right_reply.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** right_reply
+*/
+
+#include "PlayerParser.hpp"
+
+void Gui::PlayerParser::rightReply(std::string args)
+{
+    
+}
