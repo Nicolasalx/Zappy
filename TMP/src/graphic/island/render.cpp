035b11dea07ce9bb63d7fbe32bc3120d3518a76e
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-15 19:32:22 +0200

    refactor: Update Gui::Island rendering to use Gui::RenderIsland's map scale

--- TMP/src/graphic/island/render.cpp
+++ TMP/src/graphic/island/render.cpp
@@ -12,14 +12,14 @@ void Gui::Island::drawMap(ray_info_t &rayInfo)
     for (int i = 0; i < this->_gameState->map_size.x; i++) {
         for (int j = 0; j < this->_gameState->map_size.y; j++) {
             if (rayInfo.type == ISLAND && rayInfo.x == i && rayInfo.y == j) {
-                rayInfo.box.min = (Vector3){i * Gui::MAP_SCALE - 2.2f * size, -4.2f * size, j * Gui::MAP_SCALE - 2.2f * size};
-                rayInfo.box.max = (Vector3){i * Gui::MAP_SCALE + 2.2f * size, 0.2f * size, j * Gui::MAP_SCALE + 2.2f * size};
+                rayInfo.box.min = (Vector3){i * Gui::RenderIsland::map_scale - 2.2f * size, -4.2f * size, j * Gui::RenderIsland::map_scale - 2.2f * size};
+                rayInfo.box.max = (Vector3){i * Gui::RenderIsland::map_scale + 2.2f * size, 0.2f * size, j * Gui::RenderIsland::map_scale + 2.2f * size};
                 DrawBoundingBox(rayInfo.box, GREEN);
-                this->drawModel((ModelInfo){0, (Vector3){i * Gui::MAP_SCALE, -2.0 * size, j * Gui::MAP_SCALE},
+                this->drawModel((ModelInfo){0, (Vector3){i * Gui::RenderIsland::map_scale, -2.0 * size, j * Gui::RenderIsland::map_scale},
                 (Vector3){0, 0, 0}, 0, (Vector3){2.0 * size, 2.0 * size, 2.0 * size}, GREEN});
             }
             else {
-                this->drawModel((ModelInfo){0, (Vector3){i * Gui::MAP_SCALE, -2.0 * size, j * Gui::MAP_SCALE},
+                this->drawModel((ModelInfo){0, (Vector3){i * Gui::RenderIsland::map_scale, -2.0 * size, j * Gui::RenderIsland::map_scale},
                 (Vector3){0, 0, 0}, 0, (Vector3){2.0 * size, 2.0 * size, 2.0 * size}, WHITE});
             }
         }
