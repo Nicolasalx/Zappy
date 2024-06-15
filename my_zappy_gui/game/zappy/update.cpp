ef0abeb0d140bc7bdc81d145dd82afa32a683f9a
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-15 13:04:49 +0200

    add: player redering parser

--- my_zappy_gui/game/zappy/update.cpp
+++ my_zappy_gui/game/zappy/update.cpp
@@ -14,7 +14,11 @@ void Gui::Zappy::update(std::vector<std::string> &messRecv, const Gui::Event &ev
     this->_gameData->windowY = event.windowSize.height;
     this->_gameData->rayInfo = event.rayInfo;
 
-    this->_parser.update(messRecv);
+    if (this->_gameData->dataMenu.stateGame == Gui::IN_SPECTATOR_MODE) {
+        this->_parser.update(messRecv);
+    } else if (this->_gameData->dataMenu.stateGame == Gui::IN_PLAYER_MODE) {
+        this->_playerParser.update(messRecv);
+    }
 
     this->_island.update(event);
     this->_object.update(event);
