75c6eff1a775453f4ba397dab5d99f2df8ce0f4a
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-15 19:31:31 +0200

    feat: Add support for changing island properties and scale in RenderIsland class

--- my_zappy_gui/raylib_render/src/island.cpp
+++ my_zappy_gui/raylib_render/src/island.cpp
@@ -11,15 +11,21 @@
 #include "GameConfig.hpp"
 #include <raymath.h>
 
+float Gui::RenderIsland::map_scale = Gui::MAP_SCALE;
+float Gui::RenderIsland::island_size = Gui::ISLAND_SIZE;
+
 Gui::RenderIsland::RenderIsland()
 {
     this->addModel(Gui::ISLAND_MODEL.data());
     this->addModel(Gui::ISLAND_LOW_POLY.data());
     this->addModel(Gui::ISLAND_LOW_POLY2.data());
+    this->addModel(Gui::ISLAND_PLATFORM.data(), Gui::ISLAND_TEXTURE.data());
 }
 
 int Gui::RenderIsland::selectLOD(const Gui::GameData &gameData, float x, float y)
 {
+    if (modelIndex == 1)
+        return 3;
     float distance = abs(gameData.playerPos.x - x) + abs(gameData.playerPos.y - y);
     if (distance > 500.0f)
         return 2;
@@ -28,19 +34,39 @@ int Gui::RenderIsland::selectLOD(const Gui::GameData &gameData, float x, float y
     return 0;
 }
 
+void Gui::RenderIsland::changeIslandEvent()
+{
+    if (IsKeyDown(KEY_K))
+        Gui::RenderIsland::map_scale += 1.0;
+    if (IsKeyDown(KEY_J))
+        Gui::RenderIsland::map_scale -= 1.0;
+    if (IsKeyPressed(KEY_L)) {
+        if (modelIndex == 1) {
+            modelIndex = 0;
+            Gui::RenderIsland::map_scale = Gui::MAP_SCALE;
+            island_size = ISLAND_SIZE;
+        }
+        else {
+            modelIndex = 1;
+            Gui::RenderIsland::map_scale = 20.0f;
+            island_size = 4.5f;
+        }
+    }
+}
+
 void Gui::RenderIsland::render(const Gui::GameData &gameData)
 {
     for (int i = 0; i < gameData.mapSize.x; i++) {
         for (int j = 0; j < gameData.mapSize.y; j++) {
             if (gameData.rayInfo.type == ISLAND && gameData.rayInfo.x == i && gameData.rayInfo.y == j) {
-                _box.min = (Vector3){i * Gui::MAP_SCALE - 2.2f * Gui::ISLAND_SIZE, -4.2f * Gui::ISLAND_SIZE, j * Gui::MAP_SCALE - 2.2f * Gui::ISLAND_SIZE};
-                _box.max = (Vector3){i * Gui::MAP_SCALE + 2.2f * Gui::ISLAND_SIZE, 0.2f * Gui::ISLAND_SIZE, j * Gui::MAP_SCALE + 2.2f * Gui::ISLAND_SIZE};
+                _box.min = (Vector3){i * Gui::RenderIsland::map_scale - 2.2f * Gui::RenderIsland::island_size, -4.2f * Gui::RenderIsland::island_size, j * Gui::RenderIsland::map_scale - 2.2f * Gui::RenderIsland::island_size};
+                _box.max = (Vector3){i * Gui::RenderIsland::map_scale + 2.2f * Gui::RenderIsland::island_size, 0.2f * Gui::RenderIsland::island_size, j * Gui::RenderIsland::map_scale + 2.2f * Gui::RenderIsland::island_size};
                 DrawBoundingBox(_box, GREEN);
-                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::MAP_SCALE, j * Gui::MAP_SCALE), (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
-                (Vector3){0, 0, 0}, 0, (Vector3){Gui::ISLAND_SIZE, Gui::ISLAND_SIZE, Gui::ISLAND_SIZE}, GREEN});
+                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::RenderIsland::map_scale, j * Gui::RenderIsland::map_scale), (Vector3){i * Gui::RenderIsland::map_scale, 0.0f, j * Gui::RenderIsland::map_scale},
+                (Vector3){0, 0, 0}, 0, (Vector3){Gui::RenderIsland::island_size, Gui::RenderIsland::island_size, Gui::RenderIsland::island_size}, GREEN});
             } else {
-                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::MAP_SCALE, j * Gui::MAP_SCALE), (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
-                (Vector3){0, 0, 0}, 0, (Vector3){Gui::ISLAND_SIZE, Gui::ISLAND_SIZE, Gui::ISLAND_SIZE}, WHITE});
+                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::RenderIsland::map_scale, j * Gui::RenderIsland::map_scale), (Vector3){i * Gui::RenderIsland::map_scale, 0.0f, j * Gui::RenderIsland::map_scale},
+                (Vector3){0, 0, 0}, 0, (Vector3){Gui::RenderIsland::island_size, Gui::RenderIsland::island_size, Gui::RenderIsland::island_size}, WHITE});
             }
         }
     }
