8bce10886e46775f3fab39747cdbb94b45b81433
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-15 17:12:45 +0200

    add: player mode parsing in the gui

--- my_zappy_gui/game/zappy/player_parser/inventory_reply.cpp
+++ my_zappy_gui/game/zappy/player_parser/inventory_reply.cpp
@@ -6,8 +6,19 @@
 */
 
 #include "PlayerParser.hpp"
+#include <regex>
 
 void Gui::PlayerParser::inventoryReply(std::string args)
 {
+    std::array<int, 7> ressource = {0};
+    std::regex regex(R"(\b\d+\b)");
 
+    std::size_t i = 0;
+    for (auto it = std::sregex_iterator(args.begin(), args.end(), regex);
+        it != std::sregex_iterator() && i < 7; ++it) {
+        ressource[i] = std::stoi(it->str());
+        ++i;
+    }
+
+    // update inventory with "ressource" array
 }
