a9547f1b75093392a238698f2d0148c857cdf606
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-15 17:31:21 +0200

    feat: Add low-poly island models and LOD selection to RenderIsland class

--- my_zappy_gui/raylib_render/src/island.cpp
+++ my_zappy_gui/raylib_render/src/island.cpp
@@ -14,6 +14,18 @@
 Gui::RenderIsland::RenderIsland()
 {
     this->addModel(Gui::ISLAND_MODEL.data());
+    this->addModel(Gui::ISLAND_LOW_POLY.data());
+    this->addModel(Gui::ISLAND_LOW_POLY2.data());
+}
+
+int Gui::RenderIsland::selectLOD(const Gui::GameData &gameData, float x, float y)
+{
+    float distance = abs(gameData.playerPos.x - x) + abs(gameData.playerPos.y - y);
+    if (distance > 500.0f)
+        return 2;
+    if (distance > 200.0f)
+        return 1;
+    return 0;
 }
 
 void Gui::RenderIsland::render(const Gui::GameData &gameData)
@@ -24,10 +36,10 @@ void Gui::RenderIsland::render(const Gui::GameData &gameData)
                 _box.min = (Vector3){i * Gui::MAP_SCALE - 2.2f * Gui::ISLAND_SIZE, -4.2f * Gui::ISLAND_SIZE, j * Gui::MAP_SCALE - 2.2f * Gui::ISLAND_SIZE};
                 _box.max = (Vector3){i * Gui::MAP_SCALE + 2.2f * Gui::ISLAND_SIZE, 0.2f * Gui::ISLAND_SIZE, j * Gui::MAP_SCALE + 2.2f * Gui::ISLAND_SIZE};
                 DrawBoundingBox(_box, GREEN);
-                this->drawModel((ModelInfo){0, (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
+                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::MAP_SCALE, j * Gui::MAP_SCALE), (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
                 (Vector3){0, 0, 0}, 0, (Vector3){Gui::ISLAND_SIZE, Gui::ISLAND_SIZE, Gui::ISLAND_SIZE}, GREEN});
             } else {
-                this->drawModel((ModelInfo){0, (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
+                this->drawModel((ModelInfo){selectLOD(gameData, i * Gui::MAP_SCALE, j * Gui::MAP_SCALE), (Vector3){i * Gui::MAP_SCALE, 0.0f, j * Gui::MAP_SCALE},
                 (Vector3){0, 0, 0}, 0, (Vector3){Gui::ISLAND_SIZE, Gui::ISLAND_SIZE, Gui::ISLAND_SIZE}, WHITE});
             }
         }
