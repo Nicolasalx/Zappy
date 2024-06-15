2ef16485f7e021fff556c096e194da9616a2bcc4
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-15 17:12:51 +0200

    add: player mode parsing in the gui

--- my_zappy_gui/game/zappy/player_parser/look_reply.cpp
+++ my_zappy_gui/game/zappy/player_parser/look_reply.cpp
@@ -6,8 +6,27 @@
 */
 
 #include "PlayerParser.hpp"
+#include "split_string.hpp"
 
 void Gui::PlayerParser::lookReply(std::string args)
 {
-    
+    std::vector<std::string> tile;
+    std::vector<std::string> tileContent;
+    std::array<std::array<int, 8>, 81> res;
+    std::array<std::string, 8> resStr = {"player", "food",
+        "linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"};
+
+    my::split_string(args, ",", tile);
+    for (std::size_t i = 0; i < tile.size(); ++i) {
+        my::split_string(tile[i], " ", tileContent);
+        for (const std::string &ressource : tileContent) {
+            for (std::size_t j = 0; j < resStr.size(); ++j) {
+                if (ressource == resStr[j]) {
+                    ++res[i][j];
+                }
+            }
+        }
+    }
+
+    // update the ressource on tile with "res" array
 }
