a7c6b9efa22554046ad256a7da5388a27dad1b3c
Guillaume <guillaume.dubuis@epitech.eu>
2024-06-17 15:18:39 +0200

    add: seg.cpp file to zappy parser

---
+++ my_zappy_gui/game/zappy/parser/seg.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** seg.cpp
+*/
+
+#include "Parser.hpp"
+
+void Gui::Parser::seg(std::vector<std::string> args)
+{
+    ;
+}
