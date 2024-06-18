72a7ba75f9806534320009b21cf2498d79479a50
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-18 13:53:49 +0200

    mod: update the system to render the slider bar in the header in the gui

--- include/gui/Core.hpp
+++ include/gui/Core.hpp
@@ -40,7 +40,7 @@ namespace Gui
         private:
             bool eventContain(const Gui::Event &eventList, const Gui::EventType &eventType);
             bool handleCoreEvent(const Gui::Event &eventList);
-            void handleGameState();
+            void handleGameState(Gui::GameData &gameData);
 
             std::shared_ptr<Gui::IRenderModule> renderModule;
             std::shared_ptr<Gui::IClient> clientModule;
