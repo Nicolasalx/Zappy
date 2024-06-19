d84280c0dcce0b8954908651c0788dfae6f6654a
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-19 16:30:46 +0200

    add: system to handle the player mode has been added in a header in the gui

--- include/gui/GameModule/PlayerMode.hpp
+++ include/gui/GameModule/PlayerMode.hpp
@@ -8,11 +8,13 @@
 #ifndef PLAYERMODE_HPP_
     #define PLAYERMODE_HPP_
 
+    #include "IClient.hpp"
+    #include <memory>
+    #include <vector>
     #include "GameData.hpp"
     #include "Event.hpp"
-    #include "InfoGame.hpp"
-    #include <memory>
-    #include "IClient.hpp"
+    #include <queue>
+    #include "PlayerParser.hpp"
 
 namespace Gui
 {
@@ -22,17 +24,32 @@ namespace Gui
             PlayerMode(std::shared_ptr<Gui::GameData> gameData, std::shared_ptr<Gui::IClient> client);
             ~PlayerMode() = default;
 
-            void addButtonPlayerMode(const std::string &contentText, Pos pos, const SizeButton &sizeButton, ActionButtonPlayer actPlayer);
-            void turnLeft();
+            void addButtonPlayerMode(const std::string &contentText, Pos pos, const SizeButton &sizeButton, std::function<void()> actPlayer);
             void initButtonPlayerMode();
             void resize(const Gui::Event &event);
             void update(const Gui::Event &event);
 
+            void setSendedMessageQueue(std::shared_ptr<std::queue<Gui::PlayerCmd>> messageQueue);
+
             void turnLeft();
+            void turnRight();
+            void forward();
+
+            void look();
+            void eject();
+            void inventory();
+            void fork();
+            void incantation();
+            void takeElement(Elements elements);
+            void setElement(Elements elements);
+
+            std::string getNameElementFromEnum(Elements elements);
+
         private:
             std::shared_ptr<Gui::GameData> _gameData;
             std::shared_ptr<Gui::IClient> _client;
-
+            std::shared_ptr<std::queue<Gui::PlayerCmd>> _messageQueue;
+            bool _hasBeenUpdatedMessQueue = false;
     };
 }
 
