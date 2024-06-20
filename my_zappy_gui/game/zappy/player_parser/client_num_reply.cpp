6188cc3943ff5702856c3b7c1ff8fbde7cdb195b
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-20 13:47:38 +0200

    add: too many player in team handling

---
+++ my_zappy_gui/game/zappy/player_parser/client_num_reply.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-PAR-4-1-zappy-thibaud.cathala
+** File description:
+** client_num_reply
+*/
+
+#include "PlayerParser.hpp"
+
+void Gui::PlayerParser::clientNumReply(std::string args,
+    std::queue<Gui::PlayerCmd> &messageQueue)
+{
+    if (args == "ko\n") {
+        while (!this->_sendedMessage.empty()) {
+            this->_sendedMessage.pop();
+        }
+        this->_sendedMessage.push(Gui::PlayerCmd::WELCOME);
+        this->_sendedMessage.push(Gui::PlayerCmd::CLIENT_NUM);
+        this->_sendedMessage.push(Gui::PlayerCmd::MAP_SIZE);
+        throw std::runtime_error("Not enought space in team\n");
+    }
+}
