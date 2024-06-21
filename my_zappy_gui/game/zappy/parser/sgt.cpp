d24aad656da7964530968eb210896405d5a04a65
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-06-21 17:19:38 +0200

    del: remove unused comment in the update of the slidebar to debug the working of this feature in the gui part

--- my_zappy_gui/game/zappy/parser/sgt.cpp
+++ my_zappy_gui/game/zappy/parser/sgt.cpp
@@ -12,6 +12,5 @@ void Gui::Parser::sgt(std::vector<std::string> args)
     if (args.size() != 1) {
         return;
     }
-    std::cout << "NEW FREQUENCY: " << args.at(0) << "\n";
     this->_gameData->timeUnit = std::stoi(args.at(0));
 }
