01e603f014310a0e85ecbb01437cbcccbd771e2e
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-22 15:25:05 +0200

    del: useless comment in the core program of the gui

--- my_zappy_gui/game/core/loop.cpp
+++ my_zappy_gui/game/core/loop.cpp
@@ -8,18 +8,6 @@
 #include "Core.hpp"
 #include "FrameRate.hpp"
 
-/*void Gui::Graphic::loop()
-{
-    auto next_frame = std::chrono::system_clock::now();
-    while (1) {
-        next_frame += std::chrono::milliseconds(1000 / 60);
-        Gui::Event listEvent = this->actual_graphic->getEvent();
-        const Gui::GameData &data = this->actual_game->update(listEvent);
-        this->actual_graphic->render(data);
-        std::this_thread::sleep_until(next_frame);
-    }
-}*/
-
 void Gui::Core::parseArgs(int argc, const char **argv)
 {
     for (int i = 1; i < argc; i++) {
