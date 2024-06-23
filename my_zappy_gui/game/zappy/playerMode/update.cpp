dc809609ed63eda2b83118b5d5ccefd6c03e7128
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-23 14:37:37 +0200

    refactor: Update PlayerMode button handling and fix mouse position check

--- my_zappy_gui/game/zappy/playerMode/update.cpp
+++ my_zappy_gui/game/zappy/playerMode/update.cpp
@@ -10,14 +10,19 @@
 
 void Gui::PlayerMode::update(const Gui::Event &event)
 {
-    if (this->_gameData->dataMenu.stateGame != IN_PLAYER_MODE && this->_gameData->dataMenu.stateGame != TRY_PLAYER_MODE) {
+    if ((this->_gameData->dataMenu.stateGame != IN_PLAYER_MODE && this->_gameData->dataMenu.stateGame != TRY_PLAYER_MODE) || (_buttonAlreadyClicked && event.isKeyDown)) {
         return;
     }
     resize(event);
+    _buttonAlreadyClicked = false;
+
     for (auto &button: this->_gameData->playerMode.buttonPlayerMode) {
         if (event.isKeyDown && Gui::TextBox::isMouseOnBox(button.button, event.mouse) && button.actPlayer != nullptr) {
             button.button.color = RED_COLOR;
             button.actPlayer();
+            _buttonAlreadyClicked = true;
+            std::cout << "UPDATE PLAYER MODE\n";
+            
         } else {
             button.button.color = WHITE_COLOR;
         }
