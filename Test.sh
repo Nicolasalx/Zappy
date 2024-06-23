068edd8ddda74bfbd8c0c72187d46e7147c99ded
thibaudcathala <thibaud.cathala@epitech.eu>
2024-06-23 12:52:58 +0200

    add: not working test on arg parsing

--- Test.sh
+++ Test.sh
@@ -153,6 +153,8 @@ test_valid_arg_server "-y 10 -c 4 -n Team1 Team2 Team3 Team4"
 test_valid_arg_server "-c 4"
 test_valid_arg_server "-c 4 -n Team1 Team2 Team3 Team4"
 test_valid_arg_server "-n Team1 Team2 Team3 Team4"
+test_valid_arg_server "-x 10 -y 10 -c 10 -p 4242 -n Team1 -f 100"
+test_valid_arg_server "-x 10 -y 10 -p 4242 -n Team1 -f 100"
 
 ## ? END TEST
 
