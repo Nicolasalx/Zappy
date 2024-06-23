3133dfe4c8e3ee294471463d1f645b0bd4cf45cf
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-23 19:31:44 +0200

    refactor: Update RenderObject to use std::array for object_padding

--- include/gui/RenderModule/RayLibRender/RenderObject.hpp
+++ include/gui/RenderModule/RayLibRender/RenderObject.hpp
@@ -25,7 +25,7 @@ namespace Gui
 
             void render(const GameData &gameData) override;
 
-            float object_padding[7][2];
+            std::array<std::array<float, 7>, 2> object_padding;
     };
 }
 
