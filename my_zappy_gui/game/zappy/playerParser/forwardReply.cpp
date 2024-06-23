116bb90db63e64e9b41bd51f9443dad1ce5235d0
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-23 16:37:49 +0200

    Move player forward based on orientation in forwardReply function

--- my_zappy_gui/game/zappy/playerParser/forwardReply.cpp
+++ my_zappy_gui/game/zappy/playerParser/forwardReply.cpp
@@ -13,13 +13,23 @@ void Gui::PlayerParser::forwardReply(const std::string &args)
         std::cout << "ANIMATION FORWARD\n";
         if (_gameData->playerList.empty())
             return;
-        if (_gameData->playerList[0].orientation == 0)
-            _gameData->playerList[0].pos.x -= 1;
         if (_gameData->playerList[0].orientation == 1)
-            _gameData->playerList[0].pos.y += 1;
+            _gameData->playerList[0].pos.y -= 1;
         if (_gameData->playerList[0].orientation == 2)
             _gameData->playerList[0].pos.x += 1;
         if (_gameData->playerList[0].orientation == 3)
-            _gameData->playerList[0].pos.y -= 1;
+            _gameData->playerList[0].pos.y += 1;
+        if (_gameData->playerList[0].orientation == 4)
+            _gameData->playerList[0].pos.x -= 1;
+        _gameData->playerList[0].animation_nbr = 0;
+        _gameData->playerList[0].anim_frame_counter = 152;
+        if (_gameData->playerList[0].pos.x > _gameData->mapSize.x - 1)
+            _gameData->playerList[0].pos.x = 0;
+        if (_gameData->playerList[0].pos.y > _gameData->mapSize.y - 1)
+            _gameData->playerList[0].pos.y = 0;
+        if (_gameData->playerList[0].pos.x < 0)
+            _gameData->playerList[0].pos.x = _gameData->mapSize.x - 1;
+        if (_gameData->playerList[0].pos.y < 0)
+            _gameData->playerList[0].pos.y = _gameData->mapSize.y - 1;
     }
 }
