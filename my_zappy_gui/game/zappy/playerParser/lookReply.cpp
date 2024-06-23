e4cfa7ed2ae7d638647c5f3d13ebf63d3972fa01
Ulysse <ulysse.rousselet@epitech.eu>
2024-06-23 19:09:43 +0200

    refactor: Clean up lookReply function and extract helper functions

--- my_zappy_gui/game/zappy/playerParser/lookReply.cpp
+++ my_zappy_gui/game/zappy/playerParser/lookReply.cpp
@@ -8,6 +8,52 @@
 #include "PlayerParser.hpp"
 #include "split_string.hpp"
 
+static void cleanObjectPos(std::vector<std::vector<std::vector<int>>> &objectPos)
+{
+    for (std::size_t i = 0; i < objectPos.size(); ++i) {
+        for (std::size_t j = 0; j < objectPos[i].size(); ++j) {
+            for (std::size_t k = 0; k < objectPos[i][j].size(); ++k) {
+                objectPos[i][j][k] = 0;
+            }
+        }
+    }
+}
+
+static void placeObject(std::vector<Gui::PlayerData> &playerList, std::array<std::array<int, 8>, 81> &res,
+std::vector<std::vector<std::vector<int>>> &objectPos)
+{
+    for (std::size_t j = 0; j < res[1].size(); ++j) {
+        if (playerList[0].orientation == 1)
+            objectPos[playerList[0].pos.y - 1][playerList[0].pos.x - 1][j] = res[1][j + 1];
+        if (playerList[0].orientation == 2)
+            objectPos[playerList[0].pos.y - 1][playerList[0].pos.x + 1][j] = res[1][j + 1];
+        if (playerList[0].orientation == 3)
+            objectPos[playerList[0].pos.y + 1][playerList[0].pos.x + 1][j] = res[1][j + 1];
+        if (playerList[0].orientation == 4)
+            objectPos[playerList[0].pos.y + 1][playerList[0].pos.x - 1][j] = res[1][j + 1];
+    }
+    for (std::size_t j = 0; j < res[2].size(); ++j) {
+        if (playerList[0].orientation == 1)
+            objectPos[playerList[0].pos.y - 1][playerList[0].pos.x][j] = res[2][j + 1];
+        if (playerList[0].orientation == 2)
+            objectPos[playerList[0].pos.y][playerList[0].pos.x + 1][j] = res[2][j + 1];
+        if (playerList[0].orientation == 3)
+            objectPos[playerList[0].pos.y + 1][playerList[0].pos.x][j] = res[2][j + 1];
+        if (playerList[0].orientation == 4)
+            objectPos[playerList[0].pos.y][playerList[0].pos.x - 1][j] = res[2][j + 1];
+    }
+    for (std::size_t j = 0; j < res[3].size(); ++j) {
+        if (playerList[0].orientation == 1)
+            objectPos[playerList[0].pos.y - 1][playerList[0].pos.x - 1][j] = res[3][j + 1];
+        if (playerList[0].orientation == 2)
+            objectPos[playerList[0].pos.y - 1][playerList[0].pos.x + 1][j] = res[3][j + 1];
+        if (playerList[0].orientation == 3)
+            objectPos[playerList[0].pos.y + 1][playerList[0].pos.x + 1][j] = res[3][j + 1];
+        if (playerList[0].orientation == 4)
+            objectPos[playerList[0].pos.y + 1][playerList[0].pos.x - 1][j] = res[3][j + 1];
+    }
+}
+
 void Gui::PlayerParser::lookReply(const std::string &args)
 {
     std::vector<std::string> tile;
@@ -33,50 +79,17 @@ void Gui::PlayerParser::lookReply(const std::string &args)
         }
         std::cout << std::endl;
     }
-    // update the ressource on tile with "res" array
-    for (std::size_t i = 0; i < _gameData->objectPos.size(); ++i) {
-        for (std::size_t j = 0; j < _gameData->objectPos[i].size(); ++j) {
-            for (std::size_t k = 0; k < _gameData->objectPos[i][j].size(); ++k) {
-                _gameData->objectPos[i][j][k] = 0;
-            }
-        }
-    }
+
+    cleanObjectPos(_gameData->objectPos);
 
     for (std::size_t j = 0; j < res[0].size(); ++j) {
-        _gameData->objectPos[_gameData->playerList[0].pos.y][_gameData->playerList[0].pos.x][j] = res[0][j - 1];
+        _gameData->objectPos[_gameData->playerList[0].pos.y][_gameData->playerList[0].pos.x][j] = res[0][j + 1];
     }
 
-    if (_gameData->playerList[0].pos.x == 0 || _gameData->playerList[0].pos.y == 0 || _gameData->playerList[0].pos.x == _gameData->mapSize.x - 1 || _gameData->playerList[0].pos.y == _gameData->mapSize.y - 1)
+    if (_gameData->playerList[0].pos.x == 0 || _gameData->playerList[0].pos.y == 0 
+    || _gameData->playerList[0].pos.x == _gameData->mapSize.x - 1 
+    || _gameData->playerList[0].pos.y == _gameData->mapSize.y - 1)
         return;
 
-    for (std::size_t j = 0; j < res[1].size(); ++j) {
-        if (_gameData->playerList[0].orientation == 1)
-            _gameData->objectPos[_gameData->playerList[0].pos.y - 1][_gameData->playerList[0].pos.x - 1][j] = res[1][j + 1];
-        if (_gameData->playerList[0].orientation == 2)
-            _gameData->objectPos[_gameData->playerList[0].pos.y - 1][_gameData->playerList[0].pos.x + 1][j] = res[1][j + 1];
-        if (_gameData->playerList[0].orientation == 3)
-            _gameData->objectPos[_gameData->playerList[0].pos.y + 1][_gameData->playerList[0].pos.x + 1][j] = res[1][j + 1];
-        if (_gameData->playerList[0].orientation == 4)
-            _gameData->objectPos[_gameData->playerList[0].pos.y + 1][_gameData->playerList[0].pos.x - 1][j] = res[1][j + 1];
-    }
-    for (std::size_t j = 0; j < res[2].size(); ++j) {
-        if (_gameData->playerList[0].orientation == 1)
-            _gameData->objectPos[_gameData->playerList[0].pos.y - 1][_gameData->playerList[0].pos.x][j] = res[2][j + 1];
-        if (_gameData->playerList[0].orientation == 2)
-            _gameData->objectPos[_gameData->playerList[0].pos.y][_gameData->playerList[0].pos.x + 1][j] = res[2][j + 1];
-        if (_gameData->playerList[0].orientation == 3)
-            _gameData->objectPos[_gameData->playerList[0].pos.y + 1][_gameData->playerList[0].pos.x][j] = res[2][j + 1];
-        if (_gameData->playerList[0].orientation == 4)
-            _gameData->objectPos[_gameData->playerList[0].pos.y][_gameData->playerList[0].pos.x - 1][j] = res[2][j + 1];
-    }
-    for (std::size_t j = 0; j < res[3].size(); ++j) {
-        if (_gameData->playerList[0].orientation == 1)
-            _gameData->objectPos[_gameData->playerList[0].pos.y - 1][_gameData->playerList[0].pos.x - 1][j] = res[3][j + 1];
-        if (_gameData->playerList[0].orientation == 2)
-            _gameData->objectPos[_gameData->playerList[0].pos.y - 1][_gameData->playerList[0].pos.x + 1][j] = res[3][j + 1];
-        if (_gameData->playerList[0].orientation == 3)
-            _gameData->objectPos[_gameData->playerList[0].pos.y + 1][_gameData->playerList[0].pos.x + 1][j] = res[3][j + 1];
-        if (_gameData->playerList[0].orientation == 4)
-            _gameData->objectPos[_gameData->playerList[0].pos.y + 1][_gameData->playerList[0].pos.x - 1][j] = res[3][j + 1];
-    }
+    placeObject(_gameData->playerList, res, _gameData->objectPos);
 }
